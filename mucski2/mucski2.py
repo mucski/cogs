4c4ffb211e89089d01283ac2ca10e5eb934e8e72
mucski <music166@gmail.com>
2020-04-22 20:41:03 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -9,7 +9,7 @@ import time
 from redbot.core import bank, checks, commands, Config
 from redbot.core.utils.chat_formatting import bold, box, humanize_list, humanize_number, pagify
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
-from redbot.core.utils.predicates import MessagePredicates
+from redbot.core.utils.predicates import MessagePredicate
 
 class Mucski2(commands.Cog):
     def __init__(self, bot):
