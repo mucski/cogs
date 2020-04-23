268f2a045f806349e93d0743e2dff818d3c915e1
mucski <music166@gmail.com>
2020-04-23 14:19:12 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -28,8 +28,8 @@ class Mucski(commands.Cog):
         self.conf.register_user(**defaults)
         self.conf.register_guild(**default_guild)
     
-    async def cookie_retrieve(self, member):
-        self.conf.user(member).cookies()
+    def cookie_retrieve(self, member):
+        await self.conf.user(member).cookies()
     
     #test
     @commands.command()
