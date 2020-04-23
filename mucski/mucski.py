4a5440defa13e784442cdde5bfe7ba98e1b89c36
mucski <music166@gmail.com>
2020-04-23 14:35:31 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -29,12 +29,12 @@ class Mucski(commands.Cog):
         self.conf.register_guild(**default_guild)
     
     async def cr(self, member):
-        await self.conf.user(member).cookies()
+        self.conf.user(member).cookies()
     
     #test
     @commands.command()
     async def test(self, ctx, member: discord.Member):
-        cookies = self.cr(member)
+        cookies = await self.cr(member)
         await ctx.send(f"User has {cookies} cookies")
 
     @commands.group(name="cookie", aliases=['c', 'ce'])
