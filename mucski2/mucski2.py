503bbf627431747cb3c199df64eaefe7d448477f
mucski <music166@gmail.com>
2020-04-24 22:30:17 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -42,7 +42,7 @@ class Mucski2(commands.Cog):
         return
     
     @tasks.loop(seconds=30)
-    async def ugay(self, ctx)
+    async def ugay(self, ctx):
         await ctx.send("Say, I'm gay!")
         def check(m):
             return m.content == "I'm gay!" or m.content == "No u" and m.channel == ctx.channel
