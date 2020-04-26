c505b4ea1e7b6a1b8cfcadd63b5e8098a9dc6ddc
mucski <music166@gmail.com>
2020-04-26 03:57:39 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -32,7 +32,10 @@ class Mucski(commands.Cog):
             "hot dog":"You are working outside with a cart. Un scramble the following word ``dot goh``", 
             "cauterize": "You're a pro Paladins player. What do you buy first at match start? ", 
             "covid19": "You can't work cause of Quarantine. Kill the virus ``c _ _ _ _ _ _``",
-            "jack sparrow": "You're a famous Pirate. Who are you?"
+            "jack sparrow": "You're a famous Pirate. Who are you?", 
+            "bulbbulb": "Type the following word in reverse ``blubblub``", 
+            "dick": "You are working as a professional hunter. Shot the u out of a duck.",
+            "redbot": "I'm a bot, but do you know my name?",
         }
         defaults = {
             "cookies": 0
