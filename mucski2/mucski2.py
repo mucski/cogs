74278f3c1591a32ebe60980e0b58acc4e925be46
mucski <music166@gmail.com>
2020-04-27 01:14:40 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -61,9 +61,9 @@ class Mucski2(commands.Cog):
             await msg.clear_reactions()
             return await msg.edit(content="okay shutting down")
         elif emoji == '◀️':
-            await msg.edit(content=f"you reacted with {m.reaction}")
+            await msg.edit(content=f"you reacted with {pred.reaction}")
         elif emoji == '▶️':
-            await msg.edit(content=f"you reacted with {m.reaction}")
+            await msg.edit(content=f"you reacted with {pred.reaction}")
         await msg.remove_reaction(emoji, ctx.author)
     
     @commands.command()
