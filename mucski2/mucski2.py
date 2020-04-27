64ea1e197391bd2362295929273355512d1b2f34
mucski <music166@gmail.com>
2020-04-27 13:43:52 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -44,8 +44,7 @@ class Mucski2(commands.Cog):
         await ctx.send("Successfully sent your message")
         
     @commands.command()
-    async def emote(self, ctx, emoji: discord.Emoji):
-        emojis = await emoji.content
+    async def emote(self, ctx, emoji: discord.PartialEmoji):
         custom_emojis = re.findall(r'<:\w*:\d*>', emojis)
         custom_emojis = [int(e.split(':')[1].replace('>', '')) for e in custom_emojis]
         #matches = re.findall(r'<(a)?:.*?:(\d+)>', emoji)
