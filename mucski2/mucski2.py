41287a3cb8bcc2188e98dcf486635bd7d00da6e3
mucski <music166@gmail.com>
2020-04-27 13:56:30 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -45,7 +45,7 @@ class Mucski2(commands.Cog):
     
     @commands.command()
     async def tst(self, ctx, emoji: discord.Emoji):
-        await ctx.send(emoji)
+        await ctx.send(emoji(id,animated))
     
     @commands.command()
     async def emote(self, ctx, emoji: discord.PartialEmoji):
