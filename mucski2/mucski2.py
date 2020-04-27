57617e6ec72186b4fa2d02d2dfa57a49fcfc8ce2
mucski <music166@gmail.com>
2020-04-27 15:20:35 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -59,13 +59,7 @@ class Mucski2(commands.Cog):
             msg = await channel.fetch_message(messageid)
         except HTTPException:
             return await ctx.send("couldn't find that message")
-        r = discord.utils.get(msg.reactions, emoji='❤️')
-        if r is None:
-            return await ctx.send("There were no reactions. ")
-        for users in r.users():
-            member = ctx.guild.get_member(user.id)
-            if member:
-                users.append(user)
+        users = msg.reactions[0].users().flatten()
         await ctx.send(users)
         
     @commands.command()
