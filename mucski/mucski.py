9326e8652050c322163cb10937d1c5405b4978d9
mucski <music166@gmail.com>
2020-04-28 16:14:20 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -130,7 +130,7 @@ class Mucski(commands.Cog):
         else:
             cooling = "No"
         #build embed
-        e = discord.Embed(description=f"Profile for {member.name}", color=self.color)
+        e = discord.Embed(description=f"Profile for {member.name}", color=self.color())
         e.set_thumbnail(url=member.avatar_url)
         e.add_field(name="Cookies owned", value=f"``{cookie}``")
         e.add_field(name="Daily on cooldown", value=f"``{cooling}``")
