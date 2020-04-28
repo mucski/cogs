e73c04414fd7e517f96aaac2e2bc419365e366ae
mucski <music166@gmail.com>
2020-04-28 22:32:10 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -85,7 +85,7 @@ class Mucski2(commands.Cog):
         )
         e = discord.Embed(color = await self.color(ctx), description=description)
         e.set_thumbnail(url=ctx.bot.user.avatar_url)
-        e.set_author(name=f"{ctx.author}'s dice roll game.", icon_url=ctx.author.avatar_url)
+        e.set_author(name=f"{ctx.author}'s dice roll game.", icon_url=f"{ctx.author.avatar_url:>}")
         e.set_footer(text=datetime.datetime.utcnow())
         await ctx.send(embed=e)
         
