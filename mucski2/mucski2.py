e93cb9bb6680c289b7498e7d857c8b195397612d
mucski <music166@gmail.com>
2020-04-29 16:47:42 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -7,7 +7,7 @@ import time
 import re
 import calendar
 from discord.ext import tasks, commands
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from redbot.core import bank, checks, commands, Config
 from redbot.core.utils.chat_formatting import bold, box, humanize_list, humanize_number, pagify
@@ -28,12 +28,6 @@ class Mucski2(commands.Cog):
             "daily_stamp": 0,
         }
         self.conf.register_user(**defaults)
-        
-    async def get_time(self, member):
-        return await self.conf.user(member).datetime()
-    
-    async def set_time(self, member, time):
-        return await self.conf.user(member).datetime.set(time)
     
     #Get thet bot color for embeds 'await self.color(ctx)'
     async def color(self, ctx):
@@ -70,49 +64,16 @@ class Mucski2(commands.Cog):
         await ctx.send(member.avatar_url)
         
     @commands.command()
-    async def test(self, ctx):
-        member = ctx.author
-        timer = await self.conf.user(member).daily_cd()
-        timer = self.time_converter(timer)
-        now = calendar.timegm(datetime.utcnow().utctimetuple())
-        now = await self.set_time(member,now)
-        now2 = await self.get_time(member)
-        if timer - now2 != now:
-            cock = timer - now2
-            cock = datetime.fromtimestamp(cock)
-            date_format = "%d/%m %H:%M"
-            cock = cock.strftime(date_format)
-            await ctx.send(f"on cooldown until {cock}")
-            
-    @commands.command()
-    async def timeop(self, ctx):
-        #date_format = "%d/%m/%Y %H%M%S" #Used for a and b
-        date_format = "%d/%m %H:%M"
-        time = datetime.now()
-        the_time = time.strftime(date_format)
-        othertime = ctx.message.created_at
-        other_time = othertime.strftime(date_format)
-        #a = datetime.strptime(time, date_format)
-        #b = datetime.strptime(othertime, date_format)
-        #c = datetime.now()
-        #d= datetime.strptime('093000', '%H%M%S')        #Only for time comparison if someone need it.
-        #e= datetime.strptime('093000', '%H%M%S')
-        #delta = c - b  # Difference between time(s); you can play with a,b and c here since they are in same format
-        #gr=d==e # Comparison operator on time if needed can use (>, <, >=, <= and == etc.); time only
-        #fr=c>=b # Comparison operator on time if needed can use (>, <, >=, <= and == etc.); date and time                  
-        #days = delta.days
-        #del_sec = delta.seconds
-        now = calendar.timegm(datetime.utcnow().utctimetuple())
-        raff = await self.db.guild(guild).Raffle.all()
-        remaining = raff["Timestamp"] - now
-        #if remaining <= 0:
-            #do something
-            
-        timer = self.time_converter(timer)
-        end = calendar.timegm(ctx.message.created_at.utctimetuple()) + timer
-        fmt_end = time.strftime("%a %d %b %Y %H:%M:%S", time.gmtime(end))
-        await ctx.send(f"time now {the_time},message created at {other_time}, and this is the future{furure}")
+    async def setcd(self, ctx)
+        now = datetime.utcnow().replace(microsecond=0)
+        future = now + timedelta(hours=12)
+        await ctx.send(future.timestamp())
         
+        
+    @commands.command()
+    async def test(self, ctx):
+        
+
     @commands.command()
     async def rol(self, ctx):
         him = random.randint(1,6)
