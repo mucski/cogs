f8b193fd151bc4c76d4db833a942d8f3b3b8fae7
mucski <music166@gmail.com>
2020-04-30 08:44:59 +0200

    update mucski2.py

--- mucski2/mucski2.py
+++ mucski2/mucski2.py
@@ -108,7 +108,7 @@ class Mucski2(commands.Cog):
             f'Dealer rolled: - **{him}**\n\n'
             f'{msg}'
         )
-        e = discord.Embed(title="Casino", description=f"{description}", timestamp=timestamp)
+        e = discord.Embed(title="Casino", description=f"{description}", timestamp=datetime.utcnow().replace(microsecond))
         e.set_author(name=f"{ctx.author.name}'s dice roll game.", icon_url=f"{ctx.author.avatar_url}")
         e.set_thumbnail(url=f"{ctx.bot.user.avatar_url}")
         await ctx.send(embed=e)
