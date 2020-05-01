1dd9fa9edece4a3cadb71e4cf2972363ecb34338
mucski <music166@gmail.com>
2020-05-01 22:54:43 +0200

    update adminutils.py

--- mucski/adminutils.py
+++ mucski/adminutils.py
@@ -23,13 +23,30 @@ class AdminUtils(commands.Cog):
         await self.conf.user(member).cookies.set(final)
         await ctx.send(f"Added {amt} to {member.name} now has {cookie} cookies.")
     
-    async def del_cookie(self, ctx, member: discord.Member=None):
-        pass
+    @cookie.command()
+    @checks.is_owner()
+    async def del_cookie(self, ctx, amt: int, member: discord.Member=None):
+        if member is None:
+            member = ctx.author
+        cookie = await self.conf.user(member).cookies()
+        final = cookie - amt
+        await self.conf.user(member).cookies.set(final)
+        await ctx.send(f"Removed {amt} from {membee.name} now has {cookie}")
     
+    @cookie.command()
+    @checks.is_owner()
     async def reset_db(self, ctx):
-        pass
+        await self.config.clear_all()
+        await ctx.send("Thanos snapped the database.")
     
+    @cookie.command()
+    @checks.is_owner()
     async def reset_cd(self, ctx, member: discord.Member=None):
-        pass
+        if member is None:
+            member = ctx.author
+        await self.conf.user(member).daily_stamp.clear()
+        await self.conf.user(member).steal_stamp.clear()
+        await self.conf.user(member).work_stamp.clear()
+        await ctx.send(f"All cooldowns have been reset for 1 turn for {member.name}")
         
     
