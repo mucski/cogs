3124d31c19ece712e1a7fc609c1e7b63e238ddf0
mucski <music166@gmail.com>
2020-05-01 18:46:45 +0200

    update adminutils.py

--- mucski/adminutils.py
+++ mucski/adminutils.py
@@ -3,36 +3,26 @@ import discord
 from redbot.core import checks, commands
 
 class AdminUtils:
+    def __init__(self, ctx):
+        self.ctx = ctx
     
     def shuffle(self, word):
         word = list(word)
         random.shuffle(word)
         return ''.join(word)
      
-     
-    @commands.command()    
-    @checks.is_owner()
-    async def add_cookie(self, ctx, amt: int, member: discord.Member=None):
+    async def add_cookie(self, ctx, amt: int, member):
         if member is None:
             member = ctx.author
         await self.conf.user(member).cookies.set(amt)
         await ctx.send("Added {} to {}".format(member, amt))
     
-    
-    @commands.command()
-    @checks.is_owner()
     async def del_cookie(self, ctx, member: discord.Member=None):
         pass
     
-    
-    @commands.command()
-    @checks.is_owner()
     async def reset_db(self, ctx):
         pass
     
-    
-    @commands.command()
-    @checks.is_owner()
     async def reset_cd(self, ctx, member: discord.Member=None):
         pass
         
