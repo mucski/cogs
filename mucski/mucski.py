390c06e97efeedad566df91d8c8863dd99a0b81f
mucski <music166@gmail.com>
2020-05-01 16:02:54 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -6,7 +6,7 @@ from redbot.core import checks, commands, Config
 from redbot.core.utils.chat_formatting import box, humanize_timedelta, pagify
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 from datetime import datetime, timedelta
-from .adminutils import AdminUtils
+from .adminutils import AdminUtils()
 
 class Mucski(commands.Cog):
     def __init__(self, bot):
@@ -19,7 +19,6 @@ class Mucski(commands.Cog):
         }
         self.conf.register_user(**defaults)
         
-    AdminUtils()
         
     @commands.group(name="cookie", aliases=['c'])
     @commands.guild_only()
