76d2a51431abea911e3a3d5a6223f4ecae30de61
mucski <music166@gmail.com>
2020-05-02 00:35:14 +0200

    update randomstuff.py

--- mucski/randomstuff.py
+++ mucski/randomstuff.py
@@ -1,11 +1,12 @@
 worklist = {
     "hot dog":"You are working outside with a cart. Un scramble the following word ``dot goh``", 
-    "cauterize": "You're a pro Paladins player. What do you buy first at match start? ", 
+    "caut": "You're a pro Paladins player. What do you buy first at match start? ", 
     "covid19": "You can't work cause of Quarantine. Kill the virus ``c _ _ _ _ _ _``",
     "jack sparrow": "You're a famous Pirate. Who are you?", 
     "bulbbulb": "Type the following word in reverse ``blubblub``", 
     "dick": "You are working as a professional hunter. Shot the u out of a duck.",
-    "redbot": "I'm a bot, but do you know my name?",
+    "redbot": "I'm a bot, but do you know my name? Hint .info",
+    "android": "You found a little green man inside your phone. He says he forgot his name. Help him"
 }
 
 searchlist = {
