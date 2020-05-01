0aadb90839bb468f321ffd829f906c13c5cd2fdb
mucski <music166@gmail.com>
2020-05-01 15:45:22 +0200

    update adminutils.py

--- mucski/adminutils.py
+++ mucski/adminutils.py
@@ -2,8 +2,9 @@ import random
 import discord
 from redbot.core import checks, commands
 
-class AdminUtils(commands.Cog):
-    def __init__(self, cog)
+class AdminUtils:
+    def __init__(self, ctx):
+        self.ctx = ctx
     
     def shuffle(self, word):
         word = list(word)
