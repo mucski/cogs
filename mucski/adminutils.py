d609c04b6ddad5186c726f16fc877a98f0f4f928
mucski <music166@gmail.com>
2020-05-01 15:03:08 +0200

    Create adminutils.py

---
+++ mucski/adminutils.py
@@ -0,0 +1,36 @@
+import random
+import discord
+from redbot.core import checks, commands
+
+class AdminUtils:
+    
+    def shuffle(self, word):
+        word = list(word)
+        random.shuffle(word)
+        return ''.join(word)
+     
+     
+    @commands.command()    
+    @checks.is_owner()
+    async def add_cookie(self, ctx, member: discord.Member=None)
+        pass
+    
+    
+    @commands.command()
+    @checks.is_owner()
+    async def del_cookie(self, ctx, member: discord.Member=None)
+        pass
+    
+    
+    @commands.command()
+    @checks.is_owner()
+    async def reset_db(self, ctx):
+        pass
+    
+    
+    @commands.command()
+    @checks.is_owner()
+    async def reset_cd(self, ctx, member: discord.Member=None)
+        pass
+        
+    
