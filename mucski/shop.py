5b5eca6ded0b0e2f11470897fa5b6b67233d9337
mucski <music166@gmail.com>
2020-05-02 18:31:31 +0200

    update shop.py

--- mucski/shop.py
+++ mucski/shop.py
@@ -19,6 +19,17 @@ class Shop(commands.Cog):
     async def buy(self, ctx, item):
         if item in petlist.keys():
             value = petlist[item]['price']
+        cookie = await self.conf.user(ctx.author).cookies()
+        cookie -= value
+        if cookie < 0:
+            await ctx.send("Error")
+        else:
+            await self.conf.user(ctx.author).pets.owned.set("True")
+            await self.conf.user(ctx.author).pets.name.set(item)
+            await self.conf.user(ctx.author).pets.hunger.set(100)
+            await self.conf.user(ctx.author).pets.happiness.set(100)
+            await ctx.send(f" congrats you own {item} now, take good care of it ")
+        
             
     
     @shop.command()
