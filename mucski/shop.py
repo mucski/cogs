5875c832d9b6abce88e69416ea31530832989123
mucski <music166@gmail.com>
2020-05-02 12:56:38 +0200

    update shop.py

--- mucski/shop.py
+++ mucski/shop.py
@@ -2,7 +2,7 @@ import discord
 
 from redbot.core import commands, checks
 
-class Shop(commands.Cog);
+class Shop(commands.Cog):
     
     @commands.group(name="Shop")
     async def shop(self, ctx):
