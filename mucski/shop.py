6ae099046fc12eb4f0f503a2ebc5927b319e8a92
mucski <music166@gmail.com>
2020-05-02 18:05:29 +0200

    update shop.py

--- mucski/shop.py
+++ mucski/shop.py
@@ -9,6 +9,7 @@ class Shop(commands.Cog):
     @commands.group(name="shop")
     async def shop(self, ctx):
         e = discord.Embed()
+        e.set_author(name=f"{ctx.author.name}'s shop list", icon_url=ctx.author.avatar_url)
         for k, v in petlist.items():
             e.add_field(name=f"{k}", value=f"{v['description']}\nPrice: {v['price']}")
         await ctx.send(embed=e)
