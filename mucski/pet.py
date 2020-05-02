b7ef09afc5f87fd7a9658dbb65c835a0d681a725
mucski <music166@gmail.com>
2020-05-02 13:04:39 +0200

    update pet.py

--- mucski/pet.py
+++ mucski/pet.py
@@ -7,3 +7,20 @@ class Pet(commands.Cog):
     @commands.group(name="pet")
     async def pet(self, ctx):
         pass
+    
+    @pet.commands()
+    async def send(self, ctx):
+        pass
+    
+    @pet.commands()
+    async def feed(self, ctx):
+        pass
+    
+    @pet.commands()
+    async def play(self, ctx):
+        pass
+    
+    @pet.commands()
+    async def info(self, ctx):
+        pass
+    
