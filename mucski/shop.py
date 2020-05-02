f80ce00a3bb4d0ec3ed6abf88831eeb213ffd9f1
mucski <music166@gmail.com>
2020-05-02 14:52:50 +0200

    update shop.py

--- mucski/shop.py
+++ mucski/shop.py
@@ -1,12 +1,17 @@
 import discord
 
 from redbot.core import commands, checks
+from .randomstuff import shoplist
+from .randomstuff import petlist
 
 class Shop(commands.Cog):
     
     @commands.group(name="shop")
     async def shop(self, ctx):
-        pass
+        shop = []
+        for pet in petlist:
+            shop.append(petlist.keys())
+        await ctx.send(shop)
 
     @shop.command()
     async def buy(self, ctx, item):
