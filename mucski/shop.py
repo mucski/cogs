dbcb222e020eae027ffd06f033a6ea2183aa432a
mucski <music166@gmail.com>
2020-05-02 13:01:58 +0200

    update shop.py

--- mucski/shop.py
+++ mucski/shop.py
@@ -7,3 +7,16 @@ class Shop(commands.Cog):
     @commands.group(name="shop")
     async def shop(self, ctx):
         pass
+
+    @shop.commands()
+    async def buy(self, ctx, item):
+        pass
+    
+    @shop.commands()
+    async def sell(self, ctx, item):
+        pass
+    
+    @shop.commands()
+    async def info(self, ctx, item):
+        pass
+    
