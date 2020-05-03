a6a71f58082de4435d0e91863978dc528b3be514
mucski <music166@gmail.com>
2020-05-03 20:45:21 +0200

    update pet.py

--- mucski/pet.py
+++ mucski/pet.py
@@ -7,13 +7,8 @@ from redbot.core import commands, checks
 from .randomstuff import doggo_responses
 from redbot.core.utils.chat_formatting import humanize_timedelta
 
-class Pet(commands.Cog):
+class Pet:
     
-    @commands.group(name="pet")
-    async def pet(self, ctx):
-        pass
-    
-    @pet.command()
     async def send(self, ctx):
         now = datetime.utcnow().replace(microsecond=0)
         time = random.randint(900,3600)
@@ -22,35 +17,31 @@ class Pet(commands.Cog):
         pet_stamp = datetime.fromtimestamp(pet_stamp)
         wait = pet_stamp - now
         next_stamp = timer + now
-        on_mission = await self.conf.user(ctx.author).pets.mission()
+        on_mission = await self.conf.user(ctx.author).pet.mission()
         if on_mission is True:
             return await ctx.send(f"already in a mission {humanize_timedelta(timedelta=wait)}")
         else:
             await ctx.send(f"sent your dumb pet on an adventure for {time} seconds")
-            await self.conf.user(ctx.author).pets.mission.set(True)
+            await self.conf.user(ctx.author).pet.mission.set(True)
             await self.conf.user(ctx.author).pet_stamp.set(next_stamp.timestamp())
-            await asyncio.sleep(time)
             responses = random.choice(doggo_responses)
             await ctx.send(responses)
-            await self.conf.user(ctx.author).pets.mission.set(False)
+            await self.conf.user(ctx.author).pet.mission.set(False)
         
-    @pet.command()
     async def feed(self, ctx):
         pass
     
-    @pet.command()
     async def play(self, ctx):
         pass
     
-    @pet.command()
     async def info(self, ctx):
-        if await self.conf.user(ctx.author).pets.owned() is False:
+        if await self.conf.user(ctx.author).pet.owned() is False:
             await ctx.send("Get yourself a pet first.")
         else:
-            hunger = await self.conf.user(ctx.author).pets.hunger()
-            name = await self.conf.user(ctx.author).pets.name()
-            happiness = await self.conf.user(ctx.author).pets.happiness()
-            pettype = await self.conf.user(ctx.author).pets.type()
+            hunger = await self.conf.user(ctx.author).pet.hunger()
+            name = await self.conf.user(ctx.author).pet.name()
+            happiness = await self.conf.user(ctx.author).pet.happiness()
+            pettype = await self.conf.user(ctx.author).pet.type()
             e = discord.Embed()
             e.set_author(name=f"{ctx.author.name}'s {pettype}", icon_url=ctx.author.avatar_url)
             e.add_field(name="Pet name", value=name)
@@ -59,13 +50,12 @@ class Pet(commands.Cog):
             e.add_field(name="Pet type", value=pettype)
             await ctx.send(embed=e)
             
-    @pet.command()
     async def name(self, ctx, *, name: str):
         if len(name) > 15:
             return await ctx.send("Name too long. Keep it bellow 15 chars")
-        pet_owned = await self.conf.user(ctx.author).pets.owned()
+        pet_owned = await self.conf.user(ctx.author).pet.owned()
         if pet_owned is True:
-            await self.conf.user(ctx.author).pets.name.set(name)
+            await self.conf.user(ctx.author).pet.name.set(name)
             await ctx.send(f"your pet is called {name} now")
         else:
             await ctx.send("You dont have a pet")
