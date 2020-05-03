bd3633580ff0632bf808cfcf436c7a975e84104b
mucski <music166@gmail.com>
2020-05-03 17:42:08 +0200

    update main.py

--- mucski/main.py
+++ mucski/main.py
@@ -1,1 +1,29 @@
-Ahjansjf
+import discord
+
+class Main:
+    async def leaderboard(self, ctx):
+        """Cookieboards UwU"""
+        userinfo = await self.conf.all_users()
+        if not userinfo:
+            return await ctx.send(bold("Start playing first, then check boards."))
+        sorted_acc = sorted(userinfo.items(), key=lambda x: x[1]['cookies'], reverse=True)[:50]
+        li = []
+        for i, (user_id, account) in enumerate(sorted_acc, start=1):
+            user_obj = ctx.guild.get_member(user_id)
+            if len(user_obj.display_name) < 13:
+                li.append(f"#{i:2}. {user_obj.display_name:<13} {account['cookies']:>15} 🍪")
+            else:
+                li.append(f"#{i:2}. {user_obj.display_name[:10]:<10}... {account['cookies']:>15} 🍪")
+        text = "\n".join(li)
+        page_list=[]
+        for page_num, page in enumerate(pagify(text, delims=['\n'], page_length=1000), start=1):
+            embed=discord.Embed(
+                color=await self.color(ctx),
+                description=box(f"Cookieboards", lang="prolog") + (box(page, lang="md")),
+            )
+            embed.set_footer (
+                text=f"Page {page_num}/{math.ceil(len(text) / 1000)}",
+            )
+        page_list.append(embed)
+        return await menu(ctx, page_list, DEFAULT_CONTROLS)
+
