3aa764dbf5cac6c7d7ead7e49c893aa85c133f96
mucski <music166@gmail.com>
2020-05-04 00:15:19 +0200

    update pet.py

--- mucski/pet.py
+++ mucski/pet.py
@@ -32,7 +32,7 @@ class Pet:
             happiness = await self.conf.user(ctx.author).pet.happy()
             health -= random.randint(10,100)
             happiness -= random.randint(10,50)
-            if health == 0:
+            if health <= 0 or happiness <= 0:
                 await ctx.send(dog_responses + "Your pet died, rip.\n")
                 return await self.conf.user(ctx.author).pet.clear()
             await self.conf.user(ctx.author).pet.hunger.set(health)
