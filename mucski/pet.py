26e08cf3818d77bd6f229820f1a129fa0db6d657
mucski <music166@gmail.com>
2020-05-03 23:52:19 +0200

    update pet.py

--- mucski/pet.py
+++ mucski/pet.py
@@ -10,22 +10,34 @@ from redbot.core.utils.chat_formatting import humanize_timedelta
 class Pet:
     async def adventure(self, ctx):
         now = datetime.utcnow().replace(microsecond=0)
-        time = random.randint(900,3600)
+        time = random.randint(100,500)
         pet_timer = timedelta(seconds=time)
         pet_stamp = await self.conf.user(ctx.author).pet_stamp()
         pet_stamp = datetime.fromtimestamp(pet_stamp)
         next_stamp = pet_timer + now
         remaining = pet_stamp - now
         on_mission = await self.conf.user(ctx.author).pet.mission()
-        if on_mission:
-            return await ctx.send(f"already in a mission {humanize_timedelta(timedelta=remaining)}")
-        else:
+        if on_mission is False:
             await ctx.send(f"sent your dumb pet on an adventure for {time} seconds")
             await self.conf.user(ctx.author).pet.mission.set(True)
             await self.conf.user(ctx.author).pet_stamp.set(next_stamp.timestamp())
-            responses = random.choice(doggo_responses)
-            await ctx.send(responses)
-            await self.conf.user(ctx.author).pet.mission.set(False)
+        elif now < pet_stamp and on_mission is True:
+            await ctx.send(f"Still on a mission {humanize_timedelta(timedelta=remaining)}")
+        elif now > pet_stamp and on_mission is True:
+            dog_responses = random.choice(doggo_responses)
+            health -= random.randint(10,100)
+            happiness -= random.randint(10,50)
+            if health == 0:
+                await ctx.send("Your pet died, rip.\n"+dog_responses)
+                return await self.conf.user(ctx.author).pet.clear()
+            await self.conf.user(ctx.author).pet.hunger.set(health)
+            await self.conf.user(ctx.author).pet.happy.set(happiness)
+            await ctx.send(doggo_responses + f"Your pet lost {health} hunger and {happiness}")
+            
+            
+        responses = random.choice(doggo_responses)
+        await ctx.send(responses)
+        await self.conf.user(ctx.author).pet.mission.set(False)
         
     async def feed(self, ctx):
         pass
