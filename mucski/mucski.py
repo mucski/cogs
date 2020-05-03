f37f873e48d9e915f68cfc11df91443cc1ff6292
mucski <music166@gmail.com>
2020-05-03 23:31:55 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -55,6 +55,7 @@ class Mucski(commands.Cog):
     
     @cookie.command()
     async def work(self, ctx):
+        await Main.work(self, ctx)
     
     @cookie.command()
     async def steal(self, ctx, member: discord.Member):
