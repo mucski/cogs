1daf181a32b79d02a1d03caa937b5191b18cd913
mucski <music166@gmail.com>
2020-05-04 00:40:31 +0200

    update shop.py

--- mucski/shop.py
+++ mucski/shop.py
@@ -21,9 +21,9 @@ class Shop:
             e.add_field(name=f"{k.capitalize()} - {v['type']}", value=f"{v['description']} - Price {v['price']}")
         return await ctx.send(embed=e)
     
-    async def pet(self, ctx, pet):
-        if pet.lower() in petlist.keys():
-            value = petlist[pet]['price']
+    async def animal(self, ctx, animal):
+        if animal.lower() in petlist.keys():
+            value = petlist[animal]['price']
         else:
             return await ctx.send("pet doesnt exist")
         cookie = await self.conf.user(ctx.author).cookies()
@@ -33,11 +33,11 @@ class Shop:
         else:
             await self.conf.user(ctx.author).cookies.set(cookie)
             await self.conf.user(ctx.author).pet.owned.set(True)
-            await self.conf.user(ctx.author).pet.name.set(pet.capitalize())
+            await self.conf.user(ctx.author).pet.name.set(animal.capitalize())
             await self.conf.user(ctx.author).pet.hunger.set(100)
             await self.conf.user(ctx.author).pet.happy.set(100)
-            await self.conf.user(ctx.author).pet.type.set(pet.lower())
-            return await ctx.send(f" congrats you own {pet} now, take good care of it ")
+            await self.conf.user(ctx.author).pet.type.set(animal.lower())
+            return await ctx.send(f" congrats you own {animal} now, take good care of it ")
             
     async def item(self, ctx, item, quantity):
         if item.lower() in shoplist.keys():
