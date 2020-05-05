e8368eebe881b2c6543fc6a7b52031f12d9c22f8
Mucski <music166@gmail.com>
2020-05-05 21:38:46 +0200

    games chamged

--- mucski/games.py
+++ mucski/games.py
@@ -2,4 +2,40 @@ import discord
 from redbot.core import commands, checks
 
 class Games(commands.Cog):
-    pass
+    
+    @commands.command()
+    async def gamble(self, ctx, amt):
+        coin = await self.conf.user(ctx.author).coins()
+        user = random.randint(1,6)
+        dealer = random.randint(1,6)
+        try:
+            amt = int(amt)
+        except ValueError:
+            if amt == 'all':
+                amt = coin
+        if not amt:
+            await ctx.send("Need a bet amount")
+            return
+        if amt <= 0:
+            await ctx.send("Need more coins to play.")
+            return
+        if amt > coin:
+            await ctx.send("Not enough coins to play")
+            return
+        #Game logic
+        if user < 6 and dealer > user:
+            #you lost
+            coin -= amt
+            desc = (f"Dealer rolled {dealer} - You rolled {user}. You lose!")
+        elif user == dealer:
+            #its a tie
+            desc = (f"Dealer rolled {dealer} - You rolled {user}. It is a tie.")
+        elif dealer < 6 and user > dealer:
+            #you won
+            coin += amt
+            desc = (f"Dealer rolled {dealer} - You rolled {user}. You win!")
+        await self.conf.user(ctx.author).coins.set(coin)
+        e = discord.Embed(description = desc)
+        e.set_footer(text="You and the dealer rolls the dice. The one that has more than the other wins. You can also gamble all of your coins by typing <all> instead of a number.")
+        await ctx.send(embed=e)
+        
