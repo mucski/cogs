419fd0f56090a2c24a86d380bdb35802073806f1
Mucski <music166@gmail.com>
2020-05-05 22:35:01 +0200

    tyo

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -5,7 +5,7 @@ import math
 from datetime import datetime, timedelta
 
 from redbot.core import commands, checks, Config
-from redbot.core.utils.chat_formatting import bold, box, humanize_list, humanize_number, pagify
+from redbot.core.utils.chat_formatting import bold, box, humanize_timedelta, pagify
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 from redbot.core.utils.predicates import MessagePredicate
 
