3e8080dc1a2b219829e348e360d7fbe8bdd38f76
Mucski <music166@gmail.com>
2020-05-05 15:59:06 +0200

    a

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -1,5 +1,5 @@
 import discord
-from redbot.core import commands, Checks
+from redbot.core import commands, checks
 
 class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
     def __init__(self, bot):
@@ -23,3 +23,19 @@ class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
             await ctx.send("User have not started playing yet")
             return
         await ctx.send(f"User has {amt} of coins")
+        
+    @commands.command()
+    @checks.is_owner()
+    async def givecoin(self, ctx, amt: int, member: discord.Member=None):
+        if not member:
+            member = ctx.author
+        if not amt:
+            await ctx.send("Need a value")
+            return
+        if amt <= 0:
+            await ctx.send("Cant set bellow or equal to 0 coins")
+            return
+        amt += await self.conf.user(member).coins()
+        await self.conf.user(member).coins.set(amt)
+        await ctx.send(f"User now has {amt} coins")
+        
