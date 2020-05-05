2d5339aec23e33533de28fe43946ac503c042b5f
Mucski <music166@gmail.com>
2020-05-05 16:53:07 +0200

    hkggy

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -1,6 +1,6 @@
 import discord
 from redbot.core import commands, checks, Config
-from redbot.core.utils.chat_formatting import bold, box, humanize_list, humanize_number, pagify, randomize_color
+from redbot.core.utils.chat_formatting import bold, box, humanize_list, humanize_number, pagify, randomize_colour
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 from redbot.core.utils.predicates import MessagePredicate
 
