212a368e3488a8304e6a4ab6d7ca642bae9cb5f4
Mucski <music166@gmail.com>
2020-05-05 19:56:33 +0200

    a

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -54,3 +54,29 @@ class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
         await self.conf.user(ctx.author).coins.set(coin)
         await ctx.send(f"Well done, you earned {earned} coins for todays work.😴")
         
+    @commands.command(name="leaderboard", aliases=['lb', 'cb'])
+        async def leaderboard(self, ctx)
+            userinfo = await self.conf.all_users()
+            if not userinfo:
+                return await ctx.send(bold("Start playing first, then check boards."))
+            sorted_acc = sorted(userinfo.items(), key=lambda x: x[1]['coins'], reverse=True)[:50]
+            li = []
+            for i, (user_id, account) in enumerate(sorted_acc, start=1):
+                user_obj = ctx.guild.get_member(user_id)
+                if len(user_obj.display_name) < 13:
+                    li.append(f"#{i:2}. {user_obj.display_name:<13} {account['coins']:>15}")
+                else:
+                    li.append(f"#{i:2}. {user_obj.display_name[:10]:<10}... {account['coins']:>15}")
+            text = "\n".join(li)
+            page_list=[]
+            for page_num, page in enumerate(pagify(text, delims=['\n'], page_length=1000), start=1):
+                embed=discord.Embed(
+                    color=await self.color(ctx),
+                    description=box(f"Leaderboards", lang="prolog") + (box(page, lang="md")),
+                )
+                embed.set_footer (
+                    text=f"Page {page_num}/{math.ceil(len(text) / 1000)}",
+                )
+            page_list.append(embed)
+            return await menu(ctx, page_list, DEFAULT_CONTROLS) 
+        
