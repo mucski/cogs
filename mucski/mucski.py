cedf9d7004a72b16ccdbb5ef2ae010887899f73e
Mucski <music166@gmail.com>
2020-05-06 00:32:33 +0200

    s

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -65,9 +65,9 @@ class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
         for i, (user_id, account) in enumerate(sorted_acc, start=1):
             user_obj = ctx.guild.get_member(user_id)
             if len(user_obj.display_name) < 20:
-                li.append(f"#{i:2}. {user_obj.display_name:<20} {account['coins']:>10}")
+                li.append(f"#{i:2}. {user_obj.display_name:<20} {account['coins']:>13}")
             else:
-                li.append(f"#{i:2}. {user_obj.display_name[:17]:<17}... {account['coins']:>10}")
+                li.append(f"#{i:2}. {user_obj.display_name[:17]:<17}... {account['coins']:>13}")
         text = "\n".join(li)
         page_list=[]
         for page_num, page in enumerate(pagify(text, delims=['\n'], page_length=1000), start=1):
