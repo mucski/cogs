77ed35570d2e2891bab3089c4937061fc8bec3ac
Mucski <music166@gmail.com>
2020-05-05 15:51:30 +0200

    newlife

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -1,5 +1,25 @@
-from redbot.core import commands
+import discord
+from redbot.core import commands, Checks
 
-class Mucski(commands.Cog):
+class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.conf = Config.get_conf(self, 28484827, force_registration=True)
+        defaults = {
+            "coins": 0,
+            "w_stamp": 0,
+            "d_stamp": 0,
+            "s_stamp": 0,
+            "pets": {},
+        }
+        self.conf.register_user(**defaults)
+        
+    @commands.command()
+    async def balance(self, ctx, member: discord.Member=None):
+        if not member:
+            member = ctx.author
+        amt = await self.conf.user(member).coins()
+        if not amt:
+            await ctx.send("User have not started playing yet")
+            return
+        await ctx.send(f"User has {amt} of coins")
