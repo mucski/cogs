04afada94579f4f15f139e3e4313992449e0cc25
Mucski <music166@gmail.com>
2020-05-05 23:38:59 +0200

    sjsbdf

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -74,7 +74,7 @@ class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
         text = "\n".join(li)
         page_list=[]
         table = tabulate(text, headers=['#', 'Name', 'Coins'])
-        for page_num, page in enumerate(pagify(table, delims=['\n'], page_length=1000), start=1):
+        for page_num, page in enumerate(pagify(table, page_length=1000), start=1):
             embed=discord.Embed(
                 color=await ctx.bot.get_embed_color(location=ctx.channel),
                 description=box(table) + (box(page, lang="md")),
