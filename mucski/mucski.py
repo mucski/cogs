cc7e1c410d4c2da704899895d772499722567f60
mucski <music166@gmail.com>
2020-05-06 14:05:18 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -87,8 +87,9 @@ class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
         for page_num, page in enumerate(pagify(table, delims=['\n'], page_length=1500), start=1):
             embed=discord.Embed(
                 color=await ctx.bot.get_embed_color(location=ctx.channel),
-                description=f"**LEADERBOARD**\n" + page,
+                description=f"Laderboards for {ctx.guild.name}",
             )
+            embed.add_field(name="\u200b", value=page, inline=False)
             embed.set_footer (
                 text=f"Page {page_num}/{math.ceil(len(table) / 1500)}, Earn coins by claiming daily, working, and much more. Do .help Mucski to see all the available commands.",
             )
