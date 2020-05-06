010fd5a8fcb0627e0e09457dae01c4d7073b9d3e
mucski <music166@gmail.com>
2020-05-06 13:20:52 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -91,7 +91,8 @@ class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
                 text=f"Page {page_num}/{math.ceil(len(text) / 1000)}, Earn coins by claiming daily, working, and much more. Do .help Mucski to see all the available commands.",
             )
         page_list.append(embed)
-        return await menu(ctx, page_list, DEFAULT_CONTROLS)
+        await menu(ctx, page_list, DEFAULT_CONTROLS)
+        await ctx.send(li)
         
     @coin.command()
     async def daily(self, ctx):
