fb095668850676d8ee624717b08b12b591054438
mucski <music166@gmail.com>
2020-05-06 13:18:18 +0200

    update mucski.py

--- mucski/mucski.py
+++ mucski/mucski.py
@@ -75,7 +75,7 @@ class Mucski(Pet, AdminUtils, Games, Shop, commands.Cog):
         li = []
         for i, (user_id, account) in enumerate(sorted_acc, start=1):
             user_obj = ctx.guild.get_member(user_id)
-            li.append(f"#{i:2}")
+            li.append(f"{i:2}")
             li.append(f"{account['coins']:20}") 
             li.append(f"{user_obj.display_name:>20}")
         text = "\n".join(li)
