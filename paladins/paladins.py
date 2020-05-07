511993c707f9aee7c097aaf0b7a229d5c014ea77
Mucski <music166@gmail.com>
2020-05-08 00:25:52 +0200

    a

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -24,8 +24,7 @@ class Paladins(commands.Cog):
         async with self.session.get(f"https://nonsocial.herokuapp.com/api/lastmatch?query={player}&platform={platform}") as r:
             text = await r.text()
             text = text.split("|")
-            text = text.flatten()
-            text = text.replace("(", "").replace(")", "")
+            text = re.sub("\(\)", "", text)
             await ctx.send(text)
             
     # shit rewrite
