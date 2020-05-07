d26c4bb73c4e0c8985dae78ca5dd833ee293d087
Mucski <music166@gmail.com>
2020-05-07 23:19:48 +0200

    a

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -1,6 +1,7 @@
 import aiohttp
 import discord
 from redbot.core import commands, Config
+from redbot.core.utils.predicates import MessagePredicate
 
 class Paladins(commands.Cog):
     def __init__(self, bot):
@@ -12,7 +13,13 @@ class Paladins(commands.Cog):
           
     # shit rewrite
     @commands.command()
-    async def last(self, ctx, *, player, platform="pc"):
+    async def last(self, ctx):
+        msg1 = await ctx.send("Enter your paladins username")
+        await bot.wait_for("msg1", check=MessagePredicate.same_context(ctx))
+        msg2 = await ctx.send("Now enter your platform e.g pc, xbox, switch, ps4")
+        await bot.wait_for("msg2", check=MessagePredicate.same_context(ctx))
+        player = msg1.content
+        platform = msg2.content
         async with self.session.get(f"https://nonsocial.herokuapp.com/api/lastmatch?query={player}&platform={platform}") as r:
             text = await r.text()
             text = text.split('|')
