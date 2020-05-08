c9438e104bcb4e6a2ab1dc5e9435764b4292690f
Mucski <music166@gmail.com>
2020-05-08 03:36:43 +0200

    s

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -21,7 +21,7 @@ class Paladins(commands.Cog):
             text = await r.text()
             text = re.sub('[():]', '', text)
             text = text.split("|")
-            desc = ', '.join.(text)
+            desc = ', '.join(str(text)[1:-1])
             #build embed
             e = discord.Embed(
                 color=await self.bot.get_embed_color(ctx),
