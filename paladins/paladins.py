ed4b512380c94bdf06ea7ecec019387c49c295c8
Mucski <music166@gmail.com>
2020-05-08 03:42:58 +0200

    a

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -30,20 +30,4 @@ class Paladins(commands.Cog):
             e.set_author(name="Paladins stats", icon_url="https://vignette.wikia.nocookie.net/steamtradingcards/images/7/7d/Paladins_Badge_1.png/revision/latest/top-crop/width/300/height/300?cb=20161215201250")
             e.set_footer(text="Data provided by nonsocial.herokuapp.com")
             await ctx.send(embed=e)
-                
-    # shit rewrite
-    @commands.command()
-    async def champkda(self, ctx):
-        await ctx.send("Enter your paladins username")
-        msg = await self.bot.wait_for("message", check=MessagePredicate.same_context(ctx))
-        await ctx.send("Now enter the champion you wish to see the stats of")
-        msg1 = await self.bot.wait_for("message", check=MessagePredicate.same_context(ctx))
-        await ctx.send("Now enter your platform e.g pc, switch, xbox, ps4")
-        msg2 = await self.bot.wait_for("message", check=MessagePredicate.same_context(ctx))
-        player = msg.content
-        platform = msg2.content
-        champion = msg1.content
-        async with self.session.get(f"https://nonsocial.herokuapp.com/api/kda?player={player}&champion={champion}&platform={platform}") as r:
-            text = await r.text()
-            text = text.split('|')
-            await ctx.send(text)
+            
