f69fab7399839d1570f6af09d8170e122842f3a7
Mucski <music166@gmail.com>
2020-05-09 16:56:49 +0200

    penis

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -22,16 +22,17 @@ class Paladins(commands.Cog):
         async with self.session.get(f"https://nonsocial.herokuapp.com/api/lastmatch?player={player}&platform={platform}") as r:
             text = await r.text()
             text = re.findall(r"([\w ]+)\s.Match Id:\s(\d+)\s\|.Duration:\s(\d+)\w+\s\|.Region:\s(\w*)\):\s([\w\s]+)\((\d+/\d+/\d+)\s-\s(\d+).KDA\)\s.illing\sspree:\s(\d+)\s\|\sDamage:\s([\d,]+)\s\|\sCredits:\s([\d,]+)\s-\ \w+\s\(\w+:\s([\d/]+)", text)
-            test = len(text)
-            newlist = []
-            for i in range(test):
-                newlist.append(text[i])
-            #build embed
-            desc = '\n'.join(newlist)
-            e = discord.Embed(
-                color=await self.bot.get_embed_color(ctx),
-                description=f"{desc}",
-            )
+            e = discord.Embed(color=await self.bot.get_embed_color(ctx))
+            e.add_field(name="Map", value=text[1])
+            e.add_field(name="Match Id:", value=text[2])
+            e.add_field(name="Duration", value=text[3])
+            e.add_field(name="Region", value=text[4])
+            e.add_field(name="Champion", value=text[5])
+            e.add_field(name="KDA", value=f"{text[6]} ({text[7]})"
+            e.add_field(name="Kill Spree", value=text[8])
+            e.add_field(name="Damage", value=text[9])
+            e.add_field(name="Credits", value=text[10])
+            e.add_field(name="Score", value=text[11])
             e.set_author(name=f"Paladins: showing last match", icon_url="https://vignette.wikia.nocookie.net/steamtradingcards/images/7/7d/Paladins_Badge_1.png/revision/latest/top-crop/width/300/height/300?cb=20161215201250")
             e.set_footer(text="Data provided by nonsocial.herokuapp.com")
             await ctx.send(embed=e)
