53ca87269ab5a2f1dfaa1867a646ee86e4c62f6a
Mucski <music166@gmail.com>
2020-05-09 14:32:29 +0200

    ddd

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -78,4 +78,25 @@ class Paladins(commands.Cog):
             e.set_footer(text="Data provided by nonsocial.herokuapp.com")
             await ctx.send(embed=e)
             
+    @commands.command()
+    async def rank(self, ctx, player, platform="pc"):
+        async with self.session.get(f"https://nonsocial.herokuapp.com/api/rank?player={player}&platform={platform}") as r:
+            text = await r.text()
+            text = text.replace('-', '|')
+            text = re.sub('[()]', '', text)
+            text = text.split('| ')
+            test = len(text)
+            newlist = []
+            for i in range(test):
+                newlist.append(text[i])
+            #build embed
+            desc = '\n'.join(newlist)
+            e = discord.Embed(
+                color=await self.bot.get_embed_color(ctx),
+                description=f"{desc}",
+            )
+            e.set_author(name="Paladins stats", icon_url="https://vignette.wikia.nocookie.net/steamtradingcards/images/7/7d/Paladins_Badge_1.png/revision/latest/top-crop/width/300/height/300?cb=20161215201250")
+            e.set_footer(text="Data provided by nonsocial.herokuapp.com")
+            await ctx.send(embed=e)
+            
             
