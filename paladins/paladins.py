25cb5562ce5b6bb9eab9067b04c408a173d4a912
Mucski <music166@gmail.com>
2020-05-09 13:50:25 +0200

    a

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -19,7 +19,7 @@ class Paladins(commands.Cog):
         """If you have a name with space in it please use a double quote like so "john doe" """
         async with self.session.get(f"https://nonsocial.herokuapp.com/api/lastmatch?player={player}&platform={platform}") as r:
             text = await r.text()
-            text = re.sub('[():]', '', text)
+            text = re.sub('[()]', '', text)
             text = text.split('| ')
             test = len(text)
             newlist = []
@@ -34,4 +34,25 @@ class Paladins(commands.Cog):
             e.set_author(name="Paladins stats", icon_url="https://vignette.wikia.nocookie.net/steamtradingcards/images/7/7d/Paladins_Badge_1.png/revision/latest/top-crop/width/300/height/300?cb=20161215201250")
             e.set_footer(text="Data provided by nonsocial.herokuapp.com")
             await ctx.send(embed=e)
+            
+    @commands.command()
+    async def stalk(self, ctx, player, platform="pc"):
+        async with self.session.get(f"https://nonsocial.herokuapp.com/api/stalk?player={player}&platform={platform}")
+            text = await r.text()
+            text = re.sub('[()]', '', text)
+            text = text.split('| ')
+            test = len(text)
+            newlist = []
+            for i in range(test):
+                newlist.append(text[i])
+            #build embed
+            desc = '\n'.join(newlist)
+            e = discord.Embed(
+                color=await self.bot.get_embed_color(ctx),
+                description=f"{desc}",
+            )
+            e.set_author(name="Paladins stats", icon_url="https://vignette.wikia.nocookie.net/steamtradingcards/images/7/7d/Paladins_Badge_1.png/revision/latest/top-crop/width/300/height/300?cb=20161215201250")
+            e.set_footer(text="Data provided by nonsocial.herokuapp.com")
+            await ctx.send(embed=e)
+            
             
