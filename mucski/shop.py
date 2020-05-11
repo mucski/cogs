d0e148bd9ab70b1250bebdfa51a119fa2c0ba28c
mucski <music166@gmail.com>
2020-05-11 21:33:59 +0200

    update shop.py

--- mucski/shop.py
+++ mucski/shop.py
@@ -11,7 +11,12 @@ class Shop(commands.Cog):
     @shop.command()
     async def pets(self, ctx):
         for k, v in petlist.items():
-            test = k['desc']
-            await ctx.send(test)
+            #build embed
+            e = discord.Embed()
+            e.add_field("Pet", value=k)
+            e.add_field("Description", value=v['description'])
+            e.add_field("Price", value=v['price'])
+            e.add_field("Price", value=v['emoji'])
+            await ctx.send(embed=e)
             
         
