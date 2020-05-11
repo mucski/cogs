3a3ae8c8e289ef98b2dd75bca8cc7ce72214a803
Mucski <music166@gmail.com>
2020-05-11 21:15:54 +0200

    cccc

--- mucski/shop.py
+++ mucski/shop.py
@@ -1,5 +1,17 @@
 import discord
 from redbot.core import commands, checks
+from .randomstuff import petlist
 
 class Shop(commands.Cog):
-    pass
+    #list pets
+    @commands.group()
+    async def shop(self, ctx):
+        pass
+    
+    @shop.command()
+    async def pets(self, ctx):
+        for k, v in petlist.items():
+            test = petlist[k][v]
+            await ctx.send(test)
+            
+        
