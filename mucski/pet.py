fa3c0df73deae5bf71ffeafd112f7da699d01dfc
mucski <music166@gmail.com>
2020-05-11 23:43:42 +0200

    update pet.py

--- mucski/pet.py
+++ mucski/pet.py
@@ -16,6 +16,10 @@ class Pet(commands.Cog):
         if await self.conf.user(ctx.author).pets():
             await ctx.send("You already own a pet. Currently you can only have 1 pet.")
             return
+        coins -= price
+        if coins <= 0:
+            await ctx.send("Not enough coins to buy that pet.")
+            return
         async with self.conf.user(ctx.author).pets() as pet:
             pet["owned"] = True
             pet["name"] = name.capitalize()
@@ -24,12 +28,8 @@ class Pet(commands.Cog):
             pet["happy"] = 100
             pet["clean"] = 100
             pet["type"] = name.lower()
-        coins -= price
-        if coins <= 0:
-            await ctx.send("Not enough coins to buy a pet, sorry.")
-            return
-        await self.conf.user(ctx.author).coins.set(coins)
-        await ctx.send(f"You bought {pet}")
+            await self.conf.user(ctx.author).coins.set(coins)
+            await ctx.send(f"You bought {pet}")
         
     @pet.command()
     async def info(self, ctx):
