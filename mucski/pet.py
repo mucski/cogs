88d81feeee8b9dd2f8639b9b5cbfc4e2da28ece9
mucski <music166@gmail.com>
2020-05-12 23:02:03 +0200

    update pet.py

--- mucski/pet.py
+++ mucski/pet.py
@@ -83,6 +83,7 @@ class Pet(TaskHelper, commands.Cog):
         pet = await self.conf.user(ctx.author).pets()
         if not pet:
             await ctx.send("You dont have any pets")
+            return
         channel = ctx.channel
         await self.conf.user(ctx.author).channel.set(channel.id)
         petStamp = await self.conf.user(ctx.author).p_stamp()
