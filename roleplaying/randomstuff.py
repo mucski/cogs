c021c6dbad533eae087c26bbc4003d4c9b8c3cba
Mucski <music166@gmail.com>
2020-05-13 20:02:23 +0200

    testing

---
+++ roleplaying/randomstuff.py
@@ -0,0 +1,3 @@
+slaplist = [
+    "https://i.kym-cdn.com/photos/images/newsfeed/001/390/712/289.gif"
+]
---
+++ roleplaying/roleplaying.py
@@ -0,0 +1,19 @@
+import discord
+from redbot.core import commands
+from .randomstuff import slaplist
+import random
+
+class Roleplaying(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def slap(self, ctx, member: discord.Member = None):
+        img = random.choice(slaplist)
+        embed = discord.Embed()
+        if member is None or member == ctx.author:
+            embed.set_author(name=f"{ctx.author.name} slaps himself.", icon_url=ctx.author.avatar_url)
+        else:
+            embed.set_author(name=f"{ctx.author.name} slaps {member.name}", icon_url=ctx.author.avatar_url)
+        embed.image(url=img)
+        await ctx.send(embed=embed)
