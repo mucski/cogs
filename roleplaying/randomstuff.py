9041b61ea6c7fc30dc0c6f6b4eb7cd588200bd70
Mucski <music166@gmail.com>
2020-05-13 20:48:48 +0200

    testing

--- roleplaying/randomstuff.py
+++ roleplaying/randomstuff.py
@@ -4,3 +4,36 @@ slaplist = [
     "https://i.imgur.com/4MQkDKm.gif",
     "https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif",
 ]
+
+kisslist = [
+    "https://thumbs.gfycat.com/EmbellishedWeeklyGoose-size_restricted.gif",
+    "https://giphy.com/gifs/kiss-anime-riko-G3va31oEEnIkM",
+    "https://media1.tenor.com/images/0b39ba7b0affa79d3d6eddbe5bea684a/tenor.gif?itemid=15111569",
+]
+
+punchlist = [
+    "https://media1.tenor.com/images/ee3f2a6939a68df9563a7374f131fd96/tenor.gif?itemid=14210784",
+    "https://i.pinimg.com/originals/8d/50/60/8d50607e59db86b5afcc21304194ba57.gif",
+    "https://media1.tenor.com/images/6834932465e2659dc5b1ee38dfd42b44/tenor.gif?itemid=14615839",
+]
+
+huglist = [
+    "https://i.pinimg.com/originals/85/72/a1/8572a1d1ebaa45fae290e6760b59caac.gif",
+    "https://i.pinimg.com/originals/81/f1/3b/81f13bb9d62d64ad91486bf6aad75768.gif",
+    "https://i.imgur.com/niD8tPb.gif",
+    "https://i.gifer.com/Lqgh.gif",
+]
+
+patlist = [
+    "https://image.myanimelist.net/ui/OK6W_koKDTOqqqLDbIoPAseaqPawDffxoONcmCTWvsc",
+    "https://i.pinimg.com/originals/8b/42/6c/8b426c9bedc37054cd7e73925fa10da5.gif",
+    "https://steamuserimages-a.akamaihd.net/ugc/88219606963495904/F42FD6D00D4C31F95A8E13B45F7C5D634C694519/",
+    "https://i.pinimg.com/originals/70/96/0e/70960e87fb9454df6a1d15c96c9ad955.gif",
+]
+
+sadlist = [
+    "https://i.pinimg.com/originals/d6/20/f4/d620f43fcc7dba0a79712b6d41a8ed2d.gif",
+    "https://media1.tenor.com/images/addab90f2f84780e95c391750a3cefee/tenor.gif?itemid=14590288",
+    "https://66.media.tumblr.com/13ce32253c5aaa28fd66105ac436ac7f/tumblr_mv8xlhaOgn1sokegjo1_400.gifv",
+    "https://data.whicdn.com/images/339564968/original.gif",
+]
--- roleplaying/roleplaying.py
+++ roleplaying/roleplaying.py
@@ -1,6 +1,6 @@
 import discord
 from redbot.core import commands
-from .randomstuff import slaplist
+from .randomstuff import slaplist, kiclist, huglist, punchlist, sadlist, patlist, kisslist
 import random
 
 class Roleplaying(commands.Cog):
@@ -15,5 +15,57 @@ class Roleplaying(commands.Cog):
             embed.set_author(name=f"{ctx.author.name} slaps himself.", icon_url=ctx.author.avatar_url)
         else:
             embed.set_author(name=f"{ctx.author.name} slaps {member.name}", icon_url=ctx.author.avatar_url)
+        embed.set_image(url=img)
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def kiss(self, ctx, member: discord.Member = None):
+        img = random.choice(kisslist)
+        embed = discord.Embed()
+        if member is None or member == ctx.author:
+            embed.set_author(name=f"{ctx.author.name} kisses himself/herself.", icon_url=ctx.author.avatar_url)
+        else:
+            embed.set_author(name=f"{ctx.author.name} kisses {member.name}", icon_url=ctx.author.avatar_url)
+        embed.set_image(url=img)
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def pat(self, ctx, member: discord.Member = None):
+        img = random.choice(patlist)
+        embed = discord.Embed()
+        if member is None or member == ctx.author:
+            embed.set_author(name=f"{ctx.author.name} pats himself/herself.", icon_url=ctx.author.avatar_url)
+        else:
+            embed.set_author(name=f"{ctx.author.name} pats {member.name}", icon_url=ctx.author.avatar_url)
+        embed.set_image(url=img)
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def punch(self, ctx, member: discord.Member = None):
+        img = random.choice(punchlist)
+        embed = discord.Embed()
+        if member is None or member == ctx.author:
+            embed.set_author(name=f"{ctx.author.name} punches himself/herseld.", icon_url=ctx.author.avatar_url)
+        else:
+            embed.set_author(name=f"{ctx.author.name} punches {member.name}", icon_url=ctx.author.avatar_url)
+        embed.set_image(url=img)
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def hug(self, ctx, member: discord.Member = None):
+        img = random.choice(huglist)
+        embed = discord.Embed()
+        if member is None or member == ctx.author:
+            embed.set_author(name=f"{ctx.author.name} hugs himself/herself.", icon_url=ctx.author.avatar_url)
+        else:
+            embed.set_author(name=f"{ctx.author.name} hugs {member.name}", icon_url=ctx.author.avatar_url)
+        embed.set_image(url=img)
+        await ctx.send(embed=embed)
+        
+    @commands.command()
+    async def sad(self, ctx):
+        img = random.choice(sadlist)
+        embed = discord.Embed()
+        embed.set_author(name=f"{ctx.author.name} is sad.", icon_url=ctx.author.avatar_url)
         embed.set_image(url=img)
         await ctx.send(embed=embed)
