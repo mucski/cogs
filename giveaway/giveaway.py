306066d39e5dbf9ffc52dd1cb3639fe1d0fb4544
mucski <music166@gmail.com>
2020-05-13 15:24:02 +0200

    update giveaway.py

--- giveaway/giveaway.py
+++ giveaway/giveaway.py
@@ -7,7 +7,8 @@ from .taskhelper import TaskHelper
 
 class Giveaway(TaskHelper, commands.Cog):
     """Simple Giveaway Cog by Mucski"""
-    def __init__(self):
+    def __init__(self, bot):
+        self.bot = bot
         TaskHelper.__init__(self)
         self.conf = Config.get_conf(self, 975667633)
         defaults = { "channel": 0, "msg": 0, "stamp": 0, "running": False }
