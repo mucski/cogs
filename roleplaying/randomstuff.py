e19053993f1d6f08ad7824f3fef9034d785863e2
Mucski <music166@gmail.com>
2020-05-13 20:13:07 +0200

    ff

--- roleplaying/randomstuff.py
+++ roleplaying/randomstuff.py
@@ -1,7 +1,6 @@
 slaplist = [
     "https://i.kym-cdn.com/photos/images/newsfeed/001/390/712/289.gif",
     "https://pa1.narvii.com/5981/87762f909c7df15d3d6ec117be91a4e8400de133_hq.gif",
-    "https://image.myanimelist.net/ui/BQM6jEZ-UJLgGUuvrNkYUFk2Ae92E1tAeAfjk_pGLpKnHfWiikue5-m1fMe8_1TjRXlLKNwbrQTs1EfUN5ol3A",
-    "https://i.imgur.com/4MQkDKm.gif"
-    "https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif"
+    "https://i.imgur.com/4MQkDKm.gif",
+    "https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif",
 ]
