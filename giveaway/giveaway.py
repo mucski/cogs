675d600565bbe39572314e85fa0e3dc86de9b056
mucski <music166@gmail.com>
2020-05-13 15:27:37 +0200

    update giveaway.py

--- giveaway/giveaway.py
+++ giveaway/giveaway.py
@@ -13,7 +13,7 @@ class Giveaway(TaskHelper, commands.Cog):
         defaults = { "channel": 0, "msg": 0, "stamp": 0, "running": False }
         self.conf.register_guild(**defaults)
         TaskHelper.__init__(self)
-        #self.load_check = self.bot.loop.create_task(self._worker())
+        self.load_check = self.bot.loop.create_task(self._worker())
         
         @commands.group()
         async def gw(self, ctx):
