29539dce1ff4c0f5d950359be88bc7eb0f4c19e2
mucski <music166@gmail.com>
2020-05-13 13:46:01 +0200

    update giveaway.py

--- giveaway/giveaway.py
+++ giveaway/giveaway.py
@@ -1,1 +1,11 @@
-#soon
+import discord
+from redbot.core import commands
+
+from .taskhelper import TaskHelper
+
+class GiveAway(TaskHelper, commands.Cog):
+    def __init__(self):
+        TaskHelper.__init__(self)
+        self.conf = Config.get_conf(self, 975667633)
+        defaults = { "channel": 0, "msg": 0 }
+        self.conf.register_guild(**defaults)
