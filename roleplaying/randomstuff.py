bde9e392807333cdcf567c8b6d9097e6f9a41843
mucski <music166@gmail.com>
2020-05-15 00:33:42 +0200

    update randomstuff.py

--- roleplaying/randomstuff.py
+++ roleplaying/randomstuff.py
@@ -1,10 +1,3 @@
-slaplist = [
-    "https://i.kym-cdn.com/photos/images/newsfeed/001/390/712/289.gif",
-    "https://pa1.narvii.com/5981/87762f909c7df15d3d6ec117be91a4e8400de133_hq.gif",
-    "https://i.imgur.com/4MQkDKm.gif",
-    "https://i.kym-cdn.com/photos/images/newsfeed/000/940/326/086.gif",
-]
-
 kisslist = [
     "https://media.tenor.com/images/be2b3298bc9880b9ffcdc7a47635fff6/tenor.gif",
     "https://media.tenor.com/images/21fed1c94754d21acdbccd52adfb53d0/tenor.gif",
@@ -22,23 +15,11 @@ punchlist = [
     "https://media.tenor.com/images/b96f63d9382fe52cfe43feac4a8a40d6/tenor.gif",
 ]
 
-huglist = [
-    "https://i.pinimg.com/originals/85/72/a1/8572a1d1ebaa45fae290e6760b59caac.gif",
-    "https://i.pinimg.com/originals/81/f1/3b/81f13bb9d62d64ad91486bf6aad75768.gif",
-    "https://i.imgur.com/niD8tPb.gif",
-    "https://i.gifer.com/Lqgh.gif",
-]
-
-patlist = [
-    "https://image.myanimelist.net/ui/OK6W_koKDTOqqqLDbIoPAseaqPawDffxoONcmCTWvsc",
-    "https://i.pinimg.com/originals/8b/42/6c/8b426c9bedc37054cd7e73925fa10da5.gif",
-    "https://steamuserimages-a.akamaihd.net/ugc/88219606963495904/F42FD6D00D4C31F95A8E13B45F7C5D634C694519/",
-    "https://i.pinimg.com/originals/70/96/0e/70960e87fb9454df6a1d15c96c9ad955.gif",
-]
-
-sadlist = [
-    "https://i.pinimg.com/originals/d6/20/f4/d620f43fcc7dba0a79712b6d41a8ed2d.gif",
-    "https://media1.tenor.com/images/addab90f2f84780e95c391750a3cefee/tenor.gif?itemid=14590288",
-    "https://66.media.tumblr.com/13ce32253c5aaa28fd66105ac436ac7f/tumblr_mv8xlhaOgn1sokegjo1_400.gifv",
-    "https://data.whicdn.com/images/339564968/original.gif",
+cuddlelist = [
+    "https://media.tenor.com/images/735539df8adc6335bcf9b03695bfc098/tenor.gif",
+    "https://media.tenor.com/images/b62c99771c7ab751f80d2bb561c75068/tenor.gif",
+    "https://media.tenor.com/images/853bb442dd8eae619c4a524ffad4261c/tenor.gif",
+    "https://media.tenor.com/images/e101723c4fdd188e975fe6f0a6aba874/tenor.gif",
+    "https://media.tenor.com/images/d03ab0adb5173ce512f255f1e23ca399/tenor.gif",
+    "https://media.tenor.com/images/9dd8181420b4b7938463bbe69285293d/tenor.gif",
 ]
