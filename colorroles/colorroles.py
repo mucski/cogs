cecc211f6ded77129fe442d1d2ba2bd27275170c
mucski <music166@gmail.com>
2020-05-14 15:38:46 +0200

    update colorroles.py

--- colorroles/colorroles.py
+++ colorroles/colorroles.py
@@ -23,11 +23,9 @@ class ColorRoles(commands.Cog):
     @commands.command()
     async def color(self, ctx, *, color: str):
         if color in self.color_roles:
-            role = get(ctx.guild.roles, name=color)
-            for color in self.color_roles:
-                color = get(ctx.guild.roles, name=color)
-                await ctx.author.remove_roles(color)
-            await ctx.author.add_roles(role)
-            await ctx.send("Gave role {} color to {}".format(color, ctx.author))
-        else:
-            await ctx.send("That color is not yet available.")
+            roles = ctx.author.roles
+            for colors in color_roles:
+                roles.remove(colors)
+                roles.append(color)
+            await ctx.author.edit(roles=roles)
+            await ctx.send("Gave {} to {}".format(color, ctx.author))
