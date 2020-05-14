a48d8d0ea04b05c9a8adc5a86ee75e95ab64e40a
mucski <music166@gmail.com>
2020-05-14 23:59:18 +0200

    burp

--- roleplaying/roleplaying.py
+++ roleplaying/roleplaying.py
@@ -9,66 +9,15 @@ class Roleplaying(commands.Cog):
         self.bot = bot
         
     @commands.command()
-    async def slap(self, ctx, member: discord.Member = None):
-        img = random.choice(slaplist)
-        embed = discord.Embed()
-        if member is None or member == ctx.author:
-            embed.set_author(name=f"{ctx.author.name} slaps himself.", icon_url=ctx.author.avatar_url)
-        else:
-            embed.set_author(name=f"{ctx.author.name} slaps {member.name}", icon_url=ctx.author.avatar_url)
-        embed.set_image(url=img)
-        await ctx.send(embed=embed)
-        
-    @commands.command()
     async def kiss(self, ctx, member: discord.Member = None):
         img = random.choice(kisslist)
         e = discord.Embed()
         e.set_image(url=img)
         if member is None:
-            member = "mirror."
+            member = "self."
         else:
-            member = member.name
+            member = member.mention
         e.set_author(name=f"{ctx.author} kisses {member}", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=e)
         
-    @commands.command()
-    async def pat(self, ctx, member: discord.Member = None):
-        img = random.choice(patlist)
-        embed = discord.Embed()
-        if member is None or member == ctx.author:
-            embed.set_author(name=f"{ctx.author.name} pats himself/herself.", icon_url=ctx.author.avatar_url)
-        else:
-            embed.set_author(name=f"{ctx.author.name} pats {member.name}", icon_url=ctx.author.avatar_url)
-        embed.set_image(url=img)
-        await ctx.send(embed=embed)
-        
-    @commands.command()
-    async def punch(self, ctx, member: discord.Member = None):
-        img = random.choice(punchlist)
-        embed = discord.Embed()
-        if member is None or member == ctx.author:
-            embed.set_footer(text="the air.")
-        else:
-            embed.set_footer(text=f"{member.mention}")
-        embed.set_author(name=f"{ctx.author.name} punches", icon_url=ctx.author.avatar_url)
-        embed.set_image(url=img)
-        await ctx.send(embed=embed)
-        
-    @commands.command()
-    async def hug(self, ctx, member: discord.Member = None):
-        img = random.choice(huglist)
-        embed = discord.Embed()
-        if member is None or member == ctx.author:
-            embed.set_author(name=f"{ctx.author.name} hugs himself/herself.", icon_url=ctx.author.avatar_url)
-        else:
-            embed.set_author(name=f"{ctx.author.name} hugs {member.name}", icon_url=ctx.author.avatar_url)
-        embed.set_image(url=img)
-        await ctx.send(embed=embed)
-        
-    @commands.command()
-    async def sad(self, ctx):
-        img = random.choice(sadlist)
-        embed = discord.Embed()
-        embed.set_author(name=f"{ctx.author.name} is sad.", icon_url=ctx.author.avatar_url)
-        embed.set_image(url=img)
-        await ctx.send(embed=embed)
+    
