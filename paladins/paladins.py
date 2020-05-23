5147af97b041cbd9d6a6de7fc3bcf59f7fd08667
mucski <music166@gmail.com>
2020-05-23 22:38:40 +0200

    a

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -49,4 +49,5 @@ class Paladins(commands.Cog):
             e.set_author(name=f"Paladins: showing last match", icon_url="https://vignette.wikia.nocookie.net/steamtradingcards/images/7/7d/Paladins_Badge_1.png/revision/latest/top-crop/width/300/height/300?cb=20161215201250")
             e.set_footer(text="Data provided by nonsocial.herokuapp.com")
             await ctx.send(embed=e)
+            await ctx.send(avatarurl)
             
