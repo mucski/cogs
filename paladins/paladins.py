bc6e56a1c6aaf38f05dea41aa8eadeeac2005832
mucski <music166@gmail.com>
2020-05-23 22:06:44 +0200

    changes in regex

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -23,7 +23,7 @@ class Paladins(commands.Cog):
         async with self.session.get(f"https://nonsocial.herokuapp.com/api/lastmatch?player={player}&platform={platform}") as r:
             text = await r.text()
             #pain im the ass regex, domt touch
-            text = re.findall(r"([\w]+)([\w?\s]+).*Id:\s(\d+).*tion:\s(\d+)m.*ion:\s([\w\s🇺🇸]+)\W+([\w?:|\w\s?]+)\W+([\d/]+)\s-\s([\d.]+).*ee:\s(\d+).*ge:\s([\d,]+).*ts:\s([\d,]+).*ore:\s([\d/]+)", text)
+            text = re.findall(r"(\w+) ([\w? ]+) \([\w ]+: (\d+) \| \w+: (\d+)m \| \w+: ([\w 🇺🇸]+)\): ([\w ]+) \(([\d/]+) - ([\d.]+) KDA\) [\w ]+: (\d+) \| \w+: ([\d,]+) \| \w+: ([\d,]+) - \w+ \(\w+: ([\d/]+)\)", text)
             if not text:
                 await ctx.send(f':x: Wrong player name or platform. If your name contains spaces please use double quotes like so: ``.last "{player}"``')
                 return
