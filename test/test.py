a63e7ced35b3a2e2fb83ea1123166f1997cf8071
mucski <music166@gmail.com>
2020-05-24 10:03:19 +0200

    test

--- test/test.py
+++ test/test.py
@@ -1,9 +1,18 @@
 from redbot.core import commands
+from redbot.core.utils.predicates import MessagePredicate
+import random
 
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
     @commands.command()
-    async def hello(self, ctx):
-        await ctx.send("Gello World 🌍")
+    async def lockpick(self, ctx):
+        #Create the code
+        first = random.randint(0, 9)
+        second = random.randint(0, 9)
+        third = random.randint(0, 9)
+        
+        safe_lock = first + second + third
+        
+        await ctx.send(safe_lock)
