15fc44a460e190a81c608a0bf09a8ec8b8340289
mucski <music166@gmail.com>
2020-06-01 15:18:49 +0200

    update potato.py

--- potato/potato.py
+++ potato/potato.py
@@ -18,7 +18,7 @@ class Potato(commands.Cog):
     @commands.group(aliases=['p'], invoke_without_command=True)
     async def potato(self, ctx):
         async with self.db.user(ctx.author).data() as data:
-            if bool(data) is False
+            if bool(data) is False:
                 await ctx.send("Start playing first by using one of the action commands (farm, daily)")
                 return
             potato = data['potato']
