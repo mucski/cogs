3f89a41a8d6e5a991be5839aabb8292de2b95560
mucski <music166@gmail.com>
2020-06-02 02:35:27 +0200

    update potato.py

--- potato/potato.py
+++ potato/potato.py
@@ -82,7 +82,7 @@ class Potato(commands.Cog):
                     description=f"🌞 **Life**: {life}\n"
                                 f"💦 **Water**: {water}\n"
                                 f"🌱 **Growth**:\n"
-                                f"{}\n"
+                                f"{growth}\n"
                                 f"🥣 **Yield**: {0}")
                     await msg.edit(embed=new_embed)
                     await asyncio.sleep(3)
