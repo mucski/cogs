d0e3e246e84736b30f2333ebef3cc780cec794de
mucski <music166@gmail.com>
2020-06-02 13:40:28 +0200

    update potato.py

--- potato/potato.py
+++ potato/potato.py
@@ -93,7 +93,7 @@ class Potato(commands.Cog):
                     water += 10
                     if water > 100:
                         water = 100
-                    await self.bot.delete_message(m)
+                    await m.delete
     
     @potato.command()
     async def stash(self, ctx):
