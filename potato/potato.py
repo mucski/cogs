40e31123a6fd5a07069614c5ba7725e12566e43d
mucski <music166@gmail.com>
2020-06-02 04:16:48 +0200

    update potato.py

--- potato/potato.py
+++ potato/potato.py
@@ -87,6 +87,7 @@ class Potato(commands.Cog):
                     await msg.edit(embed=new_embed)
                     water -= 5
                     timeout = 2
+                    await msg.clear_reaction()
                     await asyncio.sleep(2)
                 
                 check = ReactionPredicate.with_emojis("💦", msg)
