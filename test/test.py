58100babe4d017c9b3bd79053ad2634d1091e4dc
Mucski <music166@gmail.com>
2020-06-12 16:29:52 +0200

    update

--- test/test.py
+++ test/test.py
@@ -12,6 +12,6 @@ class Test(commands.Cog):
     @commands.command()
     async def story(self, ctx):
         w = Markov(" ", 2)
-        w.learn("the quick brown fox jumps over the lazy dog")
+        w.learn(f"the quick brown fox jumps over the lazy dog {ctx.author} wtf is this shit no matter what I type here it just gives it back in the same order with nothing changed so I am not even sure what the fuck to input here don't sweat this is just a test to see if it changes and what is this I don't know")
         story = w.query()
         await ctx.send(story)
