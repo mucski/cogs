bb915a5595d0dd916f351fc60c495e6015d99f77
Mucski <music166@gmail.com>
2020-06-12 16:20:35 +0200

    markov

--- test/test.py
+++ test/test.py
@@ -3,10 +3,15 @@ from redbot.core import commands
 from redbot.core.utils.predicates import MessagePredicate
 import random
 from textwrap import dedent
+import markov
 
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
-    @commands.command()
-    async def 
+        w = markov.Markov(" ", 2)
+        w.learn("the quick brown fox jumps over the lazy dog")
+
+    @command.command()
+    async def story(ctx):
+        story = w.query()
+        await ctx.send(story)
