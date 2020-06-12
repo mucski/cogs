53ba15c7f5e8c0da33e4428c74b134485820ad7c
Mucski <music166@gmail.com>
2020-06-12 16:36:47 +0200

    testing

--- test/test.py
+++ test/test.py
@@ -8,10 +8,10 @@ from .markov import Markov
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.w = Markov(" ", 2)
+        self.w.learn("the quick brown fox jumps over the lazy dog {} wtf is this shit no matter what I type here it just gives it back in the same order with nothing changed so I am not even sure what the fuck to input here don't sweat this is just a test to see if it changes and what is this I don't know")
 
     @commands.command()
     async def story(self, ctx):
-        w = Markov(" ", 2)
-        w.learn(f"the quick brown fox jumps over the lazy dog {ctx.author} wtf is this shit no matter what I type here it just gives it back in the same order with nothing changed so I am not even sure what the fuck to input here don't sweat this is just a test to see if it changes and what is this I don't know")
-        story = w.query()
-        await ctx.send(story)
+        story = self.w.query()
+        await ctx.send(story.format(ctx.author))
