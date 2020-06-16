97e5d9d3efc48755e3ad561b8c0e54033a29eee1
Mucski <music166@gmail.com>
2020-06-16 16:36:51 +0200

    mucski

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ from redbot.core.utils.predicates import MessagePredicate
 import random
 from textwrap import dedent
 import random
-from .words import words, words2
+import .words
 
 class Test(commands.Cog):
     def __init__(self, bot):
@@ -12,6 +12,6 @@ class Test(commands.Cog):
 
     @commands.command()
     async def whoami(self, ctx):
-        words = random.choice(self.words)
-        words2 = random.choice(self.words2)
+        words = random.choice(words)
+        words2 = random.choice(words2)
         await ctx.send(f"{words} {words2}")
