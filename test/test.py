bbba029c3326dde7bc466ae0b8670533a3177b78
mucski <music166@gmail.com>
2020-11-26 18:23:30 +0100

    update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,10 @@
 from redbot.core import commands
 import random
 from .words import words, words2
+import aiohttp
+from PIL import Image, ImageDraw
+from functools import partial
+from IO import BytesIO
 
 class Test(commands.Cog):
     def __init__(self, bot):
