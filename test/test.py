278453ae4eded3291a44fdc6744a9bc05d2e62a7
mucski <music166@gmail.com>
2020-12-21 21:51:09 +0100

    update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
 from redbot.core import commands
 import random
-from .words import words, words2
+from .words import words, words2, flags
 import aiohttp
 from PIL import Image, ImageDraw
 from functools import partial
@@ -15,3 +15,9 @@ class Test(commands.Cog):
         word = random.choice(words)
         word2 = random.choice(words2)
         await ctx.send(f"{word} {word2}")
+        
+    @commands.command()
+    async def flag(self, ctx, flag):
+        orig = guild.get_member(ctx.author).nick
+        await guild.get_member(ctx.author).edit(nick=f"{flags.get(flag)} {orig}")
+        await ctx.send(f"Added {flag} to {orig}")
