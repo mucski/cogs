fa2072f70fc0340afefb5b5930ef5b9ad93ded9c
mucski <music166@gmail.com>
2020-12-21 22:09:57 +0100

    update test.py

--- test/test.py
+++ test/test.py
@@ -17,7 +17,7 @@ class Test(commands.Cog):
         
     @commands.command()
     async def flag(self, ctx, flag):
-        orig = guild.get_member(ctx.author.id).nick
+        orig = ctx.guild.get_member(author.id).nick
         #await ctx.guild.get_member(ctx.author.id).edit(nick=f"{flags.get(flag)} {orig}")
         #await ctx.send(f"Added {flag} to {orig}")
         await ctx.send(orig)
