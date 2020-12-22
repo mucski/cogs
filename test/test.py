fba2e716aa946b83c5f4b7b2296e269d3b60b24d
mucski <music166@gmail.com>
2020-12-22 23:54:09 +0100

    update test.py

--- test/test.py
+++ test/test.py
@@ -24,24 +24,23 @@ class Test(commands.Cog):
         if comp is None:
             await ctx.send("No such flag.")
             return
-        if orig.find(comp) != -1:
-            def deEmojify(text):
-                regrex_pattern = re.compile(pattern = "["
-                    u"\U0001F600-\U0001F64F"  # emoticons
-                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                                       "]+", flags = re.UNICODE)
-                return regrex_pattern.sub(r'',text)
-            new = await ctx.guild.get_member(ctx.author.id).edit(nick=f"{deEmojify(orig)}")
-            await ctx.guild.get_member(ctx.author.id).edit(nick=f"{flags.get(flag)} {new}")
-            await ctx.send("You already have a flag. I'm gonna replace it.")
-            return
+        def deEmojify(text):
+            regrex_pattern = re.compile(pattern = "["
+                u"\U0001F600-\U0001F64F"  # emoticons
+                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                   "]+", flags = re.UNICODE)
+            return regrex_pattern.sub(r'',text)
+        new = await ctx.guild.get_member(ctx.author.id).edit(nick=f"{deEmojify(orig)}")
+        await ctx.guild.get_member(ctx.author.id).edit(nick=f"{flags.get(flag)} {new}")
+        await ctx.send(f"Added {flags.get(flag)} to {orig}")
+        #await ctx.send("You already have a flag. I'm gonna replace it.")
+        #return
         #await ctx.send(orig.find(comp))
-        elif flag in flags:
-            await ctx.guild.get_member(ctx.author.id).edit(nick=f"{flags.get(flag)} {orig}")
-            await ctx.send(f"Added {flags.get(flag)} to {orig}")
-    
+        #elif flag in flags:
+            #await ctx.guild.get_member(ctx.author.id).edit(nick=f"{flags.get(flag)} {orig}")
+            
     @commands.command()
     async def delflag(self, ctx):
         orig = ctx.guild.get_member(ctx.author.id).nick
