6284575b02d136eb759bdc6c591fdcf5c77a3f6c
mucski <music166@gmail.com>
2020-12-23 00:08:02 +0100

    update test.py

--- test/test.py
+++ test/test.py
@@ -26,9 +26,9 @@ class Test(commands.Cog):
             return
         def deEmojify(text):
             regrex_pattern = re.compile(pattern = "["
-                u"\U0001F600-\U0001F64F"  # emoticons
-                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                #u"\U0001F600-\U0001F64F"  # emoticons
+                #u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                #u"\U0001F680-\U0001F6FF"  # transport & map symbols
                 u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
                                    "]+", flags = re.UNICODE)
             return regrex_pattern.sub(r'',text)
