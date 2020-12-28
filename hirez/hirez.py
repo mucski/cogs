9ffaec25f2ab21f33557d6f31e918eed89197046
mucski <music166@gmail.com>
2020-12-28 23:23:44 +0100

    update hirez.py

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -65,7 +65,7 @@ class Hirez(commands.Cog):
         
     @commands.command()
     async def hitest(self, ctx, champ):
-        await ctx.send(get_champ_image(champ))
+        await ctx.send(self.get_champ_image(champ))
 
     @commands.command()
     async def stats(self, ctx, player, platform = "pc"):
