d4ef6c8460996a4b198c8b3ae63fb6e1cfb773c8
mucski <music166@gmail.com>
2020-12-29 00:07:19 +0100

    update hirez.py

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -62,7 +62,114 @@ class Hirez(commands.Cog):
         champ_name = await self.convert_champ_name(champ_name)
         url = "https://web2.hirez.com/paladins/champion-icons/" + str(champ_name) + ".jpg"
         return url
+       
+    # Creates an team image by using champion Icons
+    async def create_team_image(champ_list, ranks):
+        champion_images = []
         
+        while len(champ_list) != 5:
+            champ_list.append("?")
+        
+        for champ in champ_list:
+            if champ != "?" and "Atlas" != champ:  # Temp fix
+                champ_url = await get_champ_image(champ)
+                response = requests.get(champ_url)
+                champion_images.append(Image.open(BytesIO(response.content)))
+            else:
+                image_size = 512
+                base = Image.new('RGB', (image_size, image_size), "black")
+        
+                # put text on image
+                base_draw = ImageDraw.Draw(base)
+                base_draw.text((128, 56), "?", font=ImageFont.truetype("arial", 400))
+                champion_images.append(base)
+        
+        # Original Image size # print(width, height)
+        image_size = 512
+        scale = 1.5
+        # champion_images.append(img.resize((image_size, image_size)))
+        
+        team_image = Image.new('RGB', (image_size * len(champion_images), image_size))
+        for i, champ in enumerate(champion_images):
+            team_image.paste(champ, (image_size*i, 0, image_size*(i+1), image_size))
+        
+            # Only try to use ranked icons if its a ranked match
+            if ranks:
+                if i < len(ranks):  # make sure we don't go out of bounds
+                    rank = Image.open("icons/ranks/" + ranks[i] + ".png")  # this works
+                    width, height = rank.size
+                    rank = rank.resize((int(width * scale), int(height * scale)))
+                    team_image.paste(rank, (0 + (image_size * i), 0), rank)  # Upper Left
+        
+        # Testing
+        # team_image.show()
+        
+        # Creates a buffer to store the image in
+        final_buffer = BytesIO()
+        
+        # Store the pillow image we just created into the buffer with the PNG format
+        team_image.save(final_buffer, "png")
+        
+        # seek back to the start of the buffer stream
+        final_buffer.seek(0)
+        
+        return final_buffer
+        
+        
+    # Creates a match image based on the two teams champions
+    async def create_match_image(team1, team2, ranks1, ranks2):
+        buffer1 = await create_team_image(team1, ranks1)
+        buffer2 = await create_team_image(team2, ranks2)
+        middle = await draw_match_vs()
+        offset = 128
+        
+        image_size = 512
+        match_image = Image.new('RGB', (image_size * len(team1), image_size*2 + offset))
+        
+        # box – The crop rectangle, as a (left, upper, right, lower)-tuple.
+        
+        # Row 1
+        match_image.paste(Image.open(buffer1), (0, 0, (image_size*len(team1)), image_size))
+        
+        # Middle row
+        match_image.paste(Image.open(middle), (0, image_size, (image_size * len(team1)), image_size+offset))
+        
+        # Row 2
+        match_image.paste(Image.open(buffer2), (0, image_size + offset, (image_size*len(team1)), image_size*2 + offset))
+        
+        # match_image.show()
+        
+        # Creates a buffer to store the image in
+        final_buffer = BytesIO()
+        
+        # Store the pillow image we just created into the buffer with the PNG format
+        match_image.save(final_buffer, "png")
+        
+        # seek back to the start of the buffer stream
+        final_buffer.seek(0)
+        
+        return final_buffer
+        
+        
+        async def draw_match_vs():
+        base = Image.new('RGB', (2560, 128), "black")
+        
+        # put text on image
+        base_draw = ImageDraw.Draw(base)
+        base_draw.text((1248, 32), "VS", font=ImageFont.truetype("arial", 64))
+        
+        # Creates a buffer to store the image in
+        final_buffer = BytesIO()
+        
+        # Store the pillow image we just created into the buffer with the PNG format
+        base.save(final_buffer, "png")
+        
+        # seek back to the start of the buffer stream
+        final_buffer.seek(0)
+        
+        return final_buffer
+        
+    
     @commands.command()
     async def hitest(self, ctx, champ_name):
         img = await self.get_champ_image(champ_name)
