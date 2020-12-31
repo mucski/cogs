a28d2c16a52e1c3fe7dcba7df46e2947c8897f7d
mucski <music166@gmail.com>
2020-12-31 17:34:46 +0100

    update hirez.py

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -144,10 +144,10 @@ class Hirez(commands.Cog):
         team2 = match.team2
         rank1 = ""
         rank2 = ""
-        #img = await create_match_image(team1, team2, rank1, rank2)
-        #file = discord.File(filename="Yourmom.png", fp=buffer)
-        #await ctx.send(file=file)
-        await ctx.send(team1)
+        img = await create_match_image(team1, team2, rank1, rank2)
+        file = discord.File(filename="Yourmom.png", fp=buffer)
+        await ctx.send(file=file)
+        #await ctx.send(team1)
         
     @commands.command()
     async def match(self, ctx, player, platform="pc"):
