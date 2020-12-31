80b1d2e660c970bf23206350cb46416f06daa18b
mucski <music166@gmail.com>
2020-12-31 13:24:30 +0100

    update hirez.py

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -135,7 +135,7 @@ class Hirez(commands.Cog):
         match = await self.api.get_match(last.id)
         players = []
         for player in match.players:
-            players.append(await player. expand_player(player))
+            players.append(await player.ranked_best)
         team1 = players
         team2 = match.team2
         rank1 = ""
