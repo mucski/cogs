ea25cde1d6aa4174123e953878d346c3be46b056
mucski <music166@gmail.com>
2021-01-06 00:21:32 +0100

    update hirez.py

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -158,6 +158,16 @@ class Hirez(commands.Cog):
                              match_player.healing_done, match_player.party_number, match_player.player.platform])
             #players.append(match_player.champion)
             #name = players.append(match_player.player.name)
+        team2data = []
+        for match_player in match.players:
+            if match_player.player.private:
+                continue
+            if match_player.team_number != 2:
+                continue
+            team2data.append([match_player.player.platform_name, match_player.account_level, match_player.credits, match_player.kda_text,
+                             match_player.damage_done, match_player.damage_taken, match_player.objective_time, match_player.damage_mitigated, 
+                             match_player.healing_done, match_player.party_number, match_player.player.platform])
+            #players.append(match_player.champion)
         ranks = []
         for match_player in match.players:
             if match_player.player.private:
