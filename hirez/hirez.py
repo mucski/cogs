aed0d3b972490c923c14d4996e07312ac4de96c1
mucski <music166@gmail.com>
2021-01-10 11:34:08 +0100

    g

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -202,7 +202,8 @@ class Hirez(commands.Cog):
             #ranks.append(match_player.player.ranked_best.rank)
         color = False
         #if colored == "-c" else False
-        buffer = await create_history_image(team1champs, team2champs, team1data, team2data, team1parties, team2parties, (match_info + temp), color)
+        buffer = await create_player_stats_image("a","b","c","d")
+        #buffer = await create_history_image(team1champs, team2champs, team1data, team2data, team1parties, team2parties, (match_info + temp), color)
         file = discord.File(filename="Yourmom.png", fp=buffer)
         await ctx.send(file=file)
         #await ctx.send(team2data)
