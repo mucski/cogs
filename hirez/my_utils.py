ec40d06a4bbee6c151f0bc48088928c3aa1dc5c4
mucski <music166@gmail.com>
2021-01-15 20:44:23 +0100

    xhgyh

--- hirez/my_utils.py
+++ hirez/my_utils.py
@@ -668,8 +668,8 @@ async def create_player_stats_image(champ_icon, champ_stats, index, party, color
     base_draw = ImageDraw.Draw(champ_stats_image)
 
     # Private account or unknown
-    if str(champ_stats[0]) == "":
-        champ_stats[0] = "*****"
+    #if str(champ_stats[0]) == "":
+        #champ_stats[0] = "*****"
 
     # Player name and champion name
     base_draw.text((img_x + 20, middle-40), str("Mucski"), font=ImageFont.truetype("/home/music166/mucski/Arial.otf", 80), fill=(0, 0, 0))
