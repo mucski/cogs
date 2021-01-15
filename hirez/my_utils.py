bb0f5a6191d57a651e45780935fa88ce7c4cd480
mucski <music166@gmail.com>
2021-01-15 21:23:43 +0100

    hubv

--- hirez/my_utils.py
+++ hirez/my_utils.py
@@ -667,7 +667,7 @@ async def create_player_stats_image(champ_icon, champ_stats, index, party, color
 
     base_draw = ImageDraw.Draw(champ_stats_image)
     
-    fnt = ImageFont.truetype("/home/music166/mucski/Arial.otf", 80)
+    fnt = ImageFont.truetype("/home/music166/mucski/Arial.otf", 80, encoding="ADBE")
 
     # Private account or unknown
     #if str(champ_stats[0]) == "":
