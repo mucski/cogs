292a0d182db8edfbe8c7822c17e39e4c1054578c
Mucski <music166@gmail.com>
2021-02-09 17:43:38 +0100

    test

--- coin/coin.py
+++ coin/coin.py
@@ -11,7 +11,7 @@ from textwrap import dedent
 import pkg_resources
 import tabulate
 from redbot.core.utils.chat_formatting import pagify, box
-from redbot.core.utils.menus import menu, DEFAULT_CONTROLStest
+from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 
 class Coin(commands.Cog):
     """Coin Tycoon game by mucski"""
