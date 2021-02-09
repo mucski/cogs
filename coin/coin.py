cf06976d025de2dafddee1ebc966c5fd850637b2
Mucski <music166@gmail.com>
2021-02-09 17:50:07 +0100

    test

--- coin/coin.py
+++ coin/coin.py
@@ -154,7 +154,7 @@ class Coin(commands.Cog):
             return await ctx.send("Start playing first, then check boards.")
         sorted_acc = sorted(userinfo.items(), key=lambda x: x[1]['coin'], reverse=True)[:50]
         li = []
-        for i, (user_id, account) in enumerate(sorted_acc, start=1):
+        for i, (user_id, account) in enumerate(sorted_acc):
             user_obj = ctx.guild.get_member(user_id)
             li.append(f"#{i:2} {user_obj.display_name:<15} {account['coin']:>15}")
         text = "\n".join(li)
