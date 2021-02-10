49b34a45ca2867d66fbb6a46f506112436a5b945
Mucski <music166@gmail.com>
2021-02-10 15:01:16 +0100

    test

--- coin/coin.py
+++ coin/coin.py
@@ -229,14 +229,6 @@ class Coin(commands.Cog):
                     break
             else:
                 key -= 1
-                e.description = (
-                    "Pick the lock using the\n"
-                    "controls bellow\n"
-                    "If you run out of picks, you lost\n"
-                    f"```{''.join(pick)}```"
-                    f"Lockpicks left: **{key}**"
-                )
-                await msg.edit(embed=e) 
             if key == 0:
                 break
             try:
