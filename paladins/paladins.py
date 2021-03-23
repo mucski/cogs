1d2814d00830aecedd5ec5dc9c35bc3ed269c76a
Mucski <music166@gmail.com>
2021-03-23 18:46:43 +0100

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -86,7 +86,7 @@ class Paladins(commands.Cog):
 
         urx, ury = llx+shorter+1, lly+shorter+1
 
-        overlay = Image.new("RBGA", out.size, TINT_COLOR+(0,))
+        overlay = Image.new("RGBA", out.size, TINT_COLOR+(0,))
         draw = Image.draw(overlay)
         draw.rectangle(((llx, lly), (urx, ury)), fill=TINT_COLOR+(OPACITY,))
         Image.alpha_composite(out, overlay)
