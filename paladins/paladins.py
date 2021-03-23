c0f9db32e0565cce0b17e60dc745d37ed01f8382
Mucski <music166@gmail.com>
2021-03-23 21:12:38 +0100

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -79,7 +79,12 @@ class Paladins(commands.Cog):
         middle = image_size_y/2 - 50
         im_color = (175, 238, 238, 0) if index % 2 == 0 else (196, 242, 242, 0)
         img = Image.new("RGBA", (img_x*9, image_size_y+offset*2), color=im_color)
-        champ_icon = Image.open(champ_icon)
+        champ_url = await self.champ_icon(champ_stats[0])
+        sessions = aiohttp.ClientSession()
+        async with sessions.get(champ_url) as response:
+            resp = await response.read()
+            champ_icon = Image.open(io.BytesIO(resp))
+        sessions.close()
         img.paste(champ_icon, (offset, offset))
         draw = ImageDraw.Draw(img)
         fnt80 = ImageFont.truetype("home/ubuntu/arial.ttf", 80)
