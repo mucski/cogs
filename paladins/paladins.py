3d4f081b6b502b206a620fc1412267e24fba90a4
Mucski <music166@gmail.com>
2021-03-24 19:09:41 +0100

    Testing

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -49,27 +49,6 @@ class Paladins(commands.Cog):
             + str(champ) + ".jpg"
         return url
 
-    async def draw_rectangle(self, img, draw):
-        TINT_COLOR = (0, 0, 0)  # Black
-        RED_COLOR = (237, 59, 59) # Red
-        BLUE_COLOR = (59, 142, 237) # Blu
-        TRANSPARENCY = .5  # Degree of transparency, 0-100%
-        OPACITY = int(255 * TRANSPARENCY)
-        # img = ImageDraw.Draw(img, "RGBA")
-        # Determine extent of the largest possible square centered on the image.
-        # and the image's shorter dimension.
-        if img.size[0] > img.size[1]:
-            shorter = img.size[1]
-            llx, lly = (img.size[0]-img.size[1]) // 2 , 0
-        else:
-            shorter = img.size[0]
-            llx, lly = 0, (img.size[1]-img.size[0]) // 2
-
-        # Calculate upper point + 1 because second point needs to be just outside the
-        # drawn rectangle when drawing rectangles.
-        urx, ury = llx+shorter+1, lly+shorter+1
-        return draw.rectangle(((llx, lly), (urx, ury)), fill=RED_COLOR+(OPACITY,))
-        # out = Image.alpha_composite(out, overlay)
 
     async def stats_image(self, champ_icon, champ_stats, index, party):
         shrink = 140
@@ -112,25 +91,233 @@ class Paladins(commands.Cog):
         return img
 
 
+    async def player_key_image(self, x, y):
+        key = Image.new("RGBA", (x*9, y-100), color=(112, 225, 255))
+        base_draw = ImageDraw.Draw(key)
+        fnt80 = ImageFont.truetype("home/ubuntu/arial.ttf", 80)
+        # fnt100 = ImageFont.truetype("home/ubuntu/arial.ttf", 100)
+        # ss = "Player Credits K/D/A  Damage  Taken  Objective Time  Shielding  Healing"
+        base_draw.text((20, 0), "Champion", font=fnt80, fill=(0, 0, 0))
+        base_draw.text((x + 20, 0), "Player", font=fnt80, fill=(0, 0, 0))
+        # Parties
+        fill = (128, 0, 128) if color else (0, 0, 0)
+        base_draw.text((x + 750, 0), "P", font=fnt80, fill=fill)
+        # Credits/Gold earned
+        fill = (218, 165, 32) if color else (0, 0, 0)
+        base_draw.text((x + 900, 0), "Credits", font=fnt80, fill=fill)
+        # KDA
+        fill = (101, 33, 67) if color else (0, 0, 0)
+        base_draw.text((x + 1300, 0), "K/D/A", font=fnt80, fill=fill)
+        # Damage done
+        fill = (255, 0, 0) if color else (0, 0, 0)
+        base_draw.text((x + 1830, 0), "Damage", font=fnt80, fill=fill)
+        # Damage taken
+        fill = (220, 20, 60) if color else (0, 0, 0)
+        base_draw.text((x + 2350, 0), "Taken", font=fnt80, fill=fill)
+        # Objective time
+        fill = (159, 105, 52) if color else (0, 0, 0)
+        base_draw.text((x + 2800, 0), "Objective", font=fnt80, fill=fill)
+        base_draw.text((x + 2850, 60), "Time", font=fnt80, fill=fill)
+        # Shielding
+        fill = (0, 51, 102) if color else (0, 0, 0)
+        base_draw.text((x + 3150, 0), "Shielding", font=fnt80, fill=fill)
+        # Healing
+        fill = (0, 128, 0) if color else (0, 0, 0)
+        base_draw.text((x + 3600, 0), "Healing", font=fnt80, fill=fill)
+
+        return key
+
+    # Creates a match image based on the two teams champions
+    async def history_image(self, team1, team2, t1_data, t2_data, p1, p2, match_data):
+        shrink = 140
+        image_size_y = 512 - shrink*2
+        image_size_x = 512
+        offset = 5
+        history_image = Image.new('RGB', (image_size_x*9, image_size_y*12 + 264))
+        # Adds the top key panel
+        key = await self.player_key_image(image_size_x, image_size_y)
+        history_image.paste(key, (0, 0))
+        # Creates middle panel
+        mid_panel = await self.middle_panel(match_data)
+        history_image.paste(mid_panel, (0, 1392-40))
+        # Adding in player data
+        for i, (champ, champ2) in enumerate(zip(team1, team2)):
+            try:
+                sessions = aiohttp.ClientSession()
+                async with sessions.get(champ) as response:
+                    resp = await response.read()
+                    champ_image = Image.open(io.BytesIO(resp))
+                sessions.close()
+            except FileNotFoundError:
+                champ_image = Image.open("icons/temp_card_art.png")
+            border = (0, shrink, 0, shrink)  # left, up, right, bottom
+            champ_image = ImageOps.crop(champ_image, border)
+            # history_image.paste(champ_image, (0, image_size*i, image_size, image_size*(i+1)))
+            player_panel = await create_player_stats_image(champ_image, t1_data[i], i, p1, colored)
+            history_image.paste(player_panel, (0, (image_size_y+10)*i+132))
+            # Second team
+            try:
+                sessions = aiohttp.ClientSession()
+                async with sessions.get(champ2) as response:
+                    resp = await response.read()
+                    champ_image = Image.open(io.BytesIO(resp))
+                sessions.close()
+            except FileNotFoundError:
+                champ_image = Image.open("icons/temp_card_art.png")
+            border = (0, shrink, 0, shrink)  # left, up, right, bottom
+            champ_image = ImageOps.crop(champ_image, border)
+
+            player_panel = await self.stats_image(champ_image, t2_data[i], i+offset-1, p2, colored)
+            history_image.paste(player_panel, (0, image_size_y * (i+offset) + 704))
+        # Base speed is 10 - seconds
+        history_image = history_image.resize((4608//2, 3048//2), Image.ANTIALIAS)           # 5 seconds
+        # history_image = history_image.resize((4608 // 4, 3048 // 4), Image.ANTIALIAS)     # 2.5 secs but bad looking
+        # Creates a buffer to store the image in
+        final_buffer = BytesIO()
+        # Store the pillow image we just created into the buffer with the PNG format
+        history_image.save(final_buffer, "png")
+        # seek back to the start of the buffer stream
+        final_buffer.seek(0)
+        return final_buffer
+
+
+    async def middle_panel(self, md):
+        middle_panel = Image.new('RGB', (512*9, 512), color=(217, 247, 247))
+        # Adding in map to image
+        map_name = map_file_name = (md[3].strip().replace("Ranked ", "").replace(" (TDM)", "").replace(" (Onslaught)", "")
+                                    .replace(" (Siege)", "")).replace("Practice ", "")
+        if "WIP" in map_name:
+            map_file_name = "test_maps"
+            map_name = map_name.replace("WIP ", "")
+        # Needed to catch weird-unknown map modes
+        try:
+            match_map = Image.open("home/ubuntu/icons/maps/{}.png".format(map_file_name.lower().replace(" ", "_").replace("'", "")))
+        except FileNotFoundError:
+            match_map = Image.open("home/ubuntu/icons/maps/test_maps.png")
+        match_map = match_map.resize((512*2, 512), Image.ANTIALIAS)
+        middle_panel.paste(match_map, (0, 0))
+        # Preparing the panel to draw on
+        draw_panel = ImageDraw.Draw(middle_panel)
+        # Add in match information
+        ds = 50  # Down Shift
+        rs = 20  # Right Shift
+        fnt100 = ImageFont.truetype("home/ubuntu/arial.ttf", 100)
+        draw_panel.text((512 * 2 + rs, 0 + ds), str(md[0]), font=fnt100, fill=(0, 0, 0))
+        draw_panel.text((512 * 2 + rs, 100 + ds), (str(md[1]) + " minutes"), font=fnt100,
+                        fill=(0, 0, 0))
+        draw_panel.text((512 * 2 + rs, 200 + ds), str(md[2]), font=fnt100, fill=(0, 0, 0))
+        draw_panel.text((512 * 2 + rs, 300 + ds), str(map_name), font=fnt100, fill=(0, 0, 0))
+        # Right shift
+        rs = 100
+        # Team 1
+        draw_panel.text((512 * 4 + rs, ds), "Team 1 Score: ", font=fnt100, fill=(0, 0, 0))
+        draw_panel.text((512 * 4 + rs * 8, ds), str(md[4]), font=fnt100, fill=(0, 0, 0))
+        center = (512/2 - 130/2)
+        center2 = (512/2 - 80/2)
+        # VS
+        draw_panel.text((512 * 5-150, center), "VS", font=fnt100, fill=(0, 0, 0))
+        # Team 2
+        draw_panel.text((512 * 4 + rs, 372), "Team 2 Score: ", font=fnt100, fill=(0, 0, 0))
+        draw_panel.text((512 * 4 + rs * 8, 372), str(md[5]), font=fnt100, fill=(0, 0, 0))
+        #  add in banned champs if it's a ranked match
+        if md[6] is not None:
+            # Ranked bans
+            draw_panel.text((512 * 5 + rs * 8, center2), "Bans:", font=fnt100, fill=(0, 0, 0))
+            # Team 1 Bans
+            try:
+                sessions = aiohttp.ClientSession()
+                async with sessions.get(md[6]) as response:
+                    resp = await response.read()
+                    champ_icon = Image.open(io.BytesIO(resp))
+                sessions.close()
+                champ_icon = champ_icon.resize((200, 200))
+                middle_panel.paste(champ_icon, (512 * 7 + rs, ds))
+            except FileNotFoundError:
+                pass
+
+            try:
+                sessions = aiohttp.ClientSession()
+                async with sessions.get(md[6]) as response:
+                    resp = await response.read()
+                    champ_icon = Image.open(io.BytesIO(resp))
+                sessions.close()
+                champ_icon = champ_icon.resize((200, 200))
+                middle_panel.paste(champ_icon, (512 * 7 + rs + 240, ds))
+            except FileNotFoundError:
+                pass
+            # Team 2 Bans
+            try:
+                sessions = aiohttp.ClientSession()
+                async with sessions.get(md[6]) as response:
+                    resp = await response.read()
+                    champ_icon = Image.open(io.BytesIO(resp))
+                sessions.close()
+                champ_icon = champ_icon.resize((200, 200))
+                middle_panel.paste(champ_icon, (512 * 7 + rs, ds+232))
+            except FileNotFoundError:
+                pass
+            try:
+                sessions = aiohttp.ClientSession()
+                async with sessions.get(md[6]) as response:
+                    resp = await response.read()
+                    champ_icon = Image.open(io.BytesIO(resp))
+                sessions.close()
+                champ_icon = champ_icon.resize((200, 200))
+                middle_panel.paste(champ_icon, (512 * 7 + rs + 240, ds+232))
+            except FileNotFoundError:
+                pass
+
+        return middle_panel
+
+
     @commands.command()
-    async def hitest(self, ctx, map):
-        # out = Image.open(f"home/ubuntu/icons/maps/{map}.png")
-        # overlay = Image.new("RGBA", out.size, TINT_COLOR+(0,))
-        # draw = ImageDraw.Draw(out, "RGBA")
-        # await self.draw_rectangle(out, draw)
-        champ_icon = await self.get_champ_image("Jenos")
-        champ_stats = ["Jenos", "Joey", "1", "4000", "24/1/24", "394923", "39394", "222", "0", "0"]
-        index = 1
-        party = 1
-        out = await self.stats_image(champ_icon, champ_stats, index, party)
-        # save it to buffer
-        buffer = io.BytesIO()
-        # save PNG in buffer
-        out.save(buffer, format='PNG')
-        # move to beginning of buffer so `send()` it will read from beginning
-        buffer.seek(0)
-        # send image
-        await ctx.send(file=File(buffer, 'yourmom.jpg'))
+    async def hitest(self, ctx, matchId):
+        match = await self.api.get_match(matchId, expand_players=True)
+        team1_data = []
+        team2_data = []
+        team1_champs = []
+        team2_champs = []
+        team1_parties = {}
+        team2_parties = {}
+        temp = []
+        new_party_id = 0
+        if match_id == -1 or match_id == match.matchId:
+            # match_data = self.bot.paladinsAPI.getMatch(match.matchId)
+            match_info = [match.winning_team, match.duration, match.region,
+                          match.map_name, match.score[0], match.score[1]]
+        for player in match:
+            temp = match.bans
+            if match.players.team_number == 1:
+                kda = match.players.kda_text
+                team1_data.append([match.players.player, match.players.account_level, match.players.credits, kda,
+                                   match.players.damage_done, match.players.damage_taken,
+                                   match.players.objective_time, match.players.damage_mitigated,
+                                   match.players.healing_done, match.players.party_number, match.players.platform])
+                team1_champs.append(match.players.champion)
+                if match.players.party_number not in team1_parties or match.players.party_number == 0:
+                    team1_parties[match.players.party_number] = ""
+                else:
+                    if team1_parties[match.players.party_number] == "":
+                        new_party_id += 1
+                        team1_parties[match.players.party_number] = "" + str(new_party_id)
+            else:
+                kda = match.players.kda_text
+                team2_data.append([match.players.player, match.players.account_level, match.players.credits, kda,
+                                   match.players.damage_done, match.players.damage_taken,
+                                   match.players.objective_time, match.players.damage_mitigated,
+                                   match.players.healing_done, match.players.party_number, match.players.platform])
+                team2_champs.append(match.players.champion)
+                if match.players.party_number not in team1_parties or match.players.party_number == 0:
+                    team1_parties[match.players.party_number] = ""
+                else:
+                    if team2_champs[match.players.party_number] == "":
+                        new_party_id += 1
+                        team2_champs[match.players.party_number] = "" + str(new_party_id)
+        buffer = await self.history_image(team1_champs, team2_champs, team1_data, team2_data,
+                                                               team1_parties, team2_parties, (match_info + temp))
+        file = discord.File(filename="TeamMatch.png", fp=buffer)
+        await ctx.send("```You are an amazing person!```", file=file)
+
 
     @commands.command()
     async def stats(self, ctx, player, platform="PC"):
