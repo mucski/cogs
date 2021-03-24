5ddc61179064a110ecbf2049bdbf57c9b77cee79
Mucski <music166@gmail.com>
2021-03-24 22:29:24 +0100

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -279,7 +279,7 @@ class Paladins(commands.Cog):
         except ValueError:
             match = await self.api.get_player(matchId)
             match = await match.get_match_history()
-            match = match[0]
+            match = await match[0]
         team1_data = []
         team2_data = []
         team1_champs = []
