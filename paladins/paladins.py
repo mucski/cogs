8b155a71832fb2f5a062b17be3ee59dbf864bf0b
Mucski <music166@gmail.com>
2021-03-24 20:11:09 +0100

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -330,7 +330,8 @@ class Paladins(commands.Cog):
                 player_info.append(player.deaths)
                 player_info.append(player.assists)
                 player_info.append(player.damage_done)
-        await ctx.send(pagify(player_info))
+        for page in pagify(player_info):
+            await ctx.send(page)
 
     @commands.command()
     async def stats(self, ctx, player, platform="PC"):
