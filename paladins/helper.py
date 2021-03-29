5d2eae7fee6314abe8cd02bae6f6a00bdd8b3db0
mucski <music166@gmail.com>
2021-03-30 00:15:54 +0200

    test

--- paladins/helper.py
+++ paladins/helper.py
@@ -36,7 +36,7 @@ class helper:
 	    im_color = (14, 34, 43) if index % 2 == 0 else (15, 40, 48)
 	    img = Image.new("RGB", (img_x*10, image_size_y+offset*2), color=im_color)
 	    img.paste(champ_icon, (offset, offset))
-	    img.paste(rank_icon, (300, middle))
+	    img.paste(rank_icon, (300, 210))
 	    draw = ImageDraw.Draw(img)
 	    fnt80 = ImageFont.truetype("home/ubuntu/arial.ttf", 80)
 	    fnt100 = ImageFont.truetype("home/ubuntu/arial.ttf", 100)
