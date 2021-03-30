549e1fb700185ff33607c44f940b4af18a9ca329
mucski <music166@gmail.com>
2021-03-30 13:31:14 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -108,7 +108,7 @@ class Paladins(commands.Cog):
 
     @commands.command()
     async def hirez(self, ctx, request):
-        data = await arez.Endpoint.request(method_name=request)
+        data = await self.arez.Endpoint.request(request)
         await ctx.send(f"```json {data} ```")
 
 
