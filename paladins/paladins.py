f89cba35be29d94596739965903043ff33871d9d
Mucski <music166@gmail.com>
2021-03-30 16:28:38 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -94,7 +94,8 @@ class Paladins(commands.Cog):
     async def hirez(self, ctx, request, data=None):
         data = await self.api.request(request, data)
         # data = await data
-        await ctx.send_interactive(pagify(f"```json\n{data}\n```"))
+        for page in pagify(data):
+            await ctx.send(f"```json\n{data}\n```")
 
 
     @commands.command()
