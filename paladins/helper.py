0144d30bb7a43e25a1b1038bb17ad36a4b3c5270
Mucski <music166@gmail.com>
2021-03-30 20:17:18 +0200

    test

--- paladins/helper.py
+++ paladins/helper.py
@@ -92,7 +92,7 @@ class helper:
 
     @classmethod
     async def player_key_image(cls, x, y):
-        key = Image.new("RGBA", (x*10, y-100), color=(8, 21, 25))
+        key = Image.new("RGB", (x*10, y-100), color=(8, 21, 25))
         base_draw = ImageDraw.Draw(key)
         fill = (255, 255, 255)
         fnt80bold = ImageFont.truetype("home/ubuntu/arialbd.ttf", 80)
@@ -132,8 +132,7 @@ class helper:
         image_size_y = 512 - shrink*2
         image_size_x = 512
         offset = 5
-        history_image = Image.new(
-            'RGB', (image_size_x*10, image_size_y*12 + 264))
+        history_image = Image.new("RGB", (image_size_x*10, image_size_y*12 + 264))
         # Adds the top key panel
         key = await helper.player_key_image(image_size_x, image_size_y)
         history_image.paste(key, (0, 0))
@@ -189,7 +188,7 @@ class helper:
 
     @classmethod
     async def middle_panel(cls, md):
-        middle_panel = Image.new('RGBA', (512*10, 512), color=(14, 52, 60))
+        middle_panel = Image.new("RGB", (512*10, 512), color=(14, 52, 60))
         # Adding in map to image
         map_name = map_file_name = (md[3].strip().replace("Ranked ", "").replace(" (TDM)", "").replace(" (Onslaught)", "")
                                     .replace(" (Siege)", "")).replace("Practice ", "")
