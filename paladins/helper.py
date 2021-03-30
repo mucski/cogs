ae406ec65461ba24e4cd97570d6f6dbe87db82b1
Mucski <music166@gmail.com>
2021-03-30 21:52:52 +0200

    test

--- paladins/helper.py
+++ paladins/helper.py
@@ -240,17 +240,17 @@ class helper:
         # Team 1
         draw_panel.text((512 * 5 + rs - 200, ds), "Team 1 Score: ",
                         font=fnt100, fill=(255, 255, 255))
-        draw_panel.text((512 * 6 - 100, ds),
+        draw_panel.text((512 * 6, ds),
                         str(md[4]), font=fnt100bold, fill=(255, 255, 255))
         center = (512/2 - 130/2)
         center2 = (512/2 - 80/2)
         # VS
-        draw_panel.text((512 * 6-150-300, 200), "VS",
+        draw_panel.text((512 * 6-350, 200), "VS",
                         font=fnt100bold, fill=(227, 34, 34))
         # Team 2
         draw_panel.text((512 * 5 + rs - 200, 348), "Team 2 Score: ",
                         font=fnt100, fill=(255, 255, 255))
-        draw_panel.text((512 * 6 - 100, 348),
+        draw_panel.text((512 * 6, 348),
                         str(md[5]), font=fnt100bold, fill=(255, 255, 255))
         #  add in banned champs if it's a ranked match
         try:
