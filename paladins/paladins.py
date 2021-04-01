df1671435dcbb58009b8af9ed1e96769999d00f5
mucski <music166@gmail.com>
2021-04-01 22:14:39 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -95,8 +95,8 @@ class Paladins(commands.Cog):
     async def hirez(self, ctx, request, *msg):
         try:
             data = await self.api.request(request, *msg)
-        except arez.HTTPException:
-            await ctx.send("Error: wrong usage or api unavaiable!")
+        except arez.HTTPException as exc:
+            await ctx.send(exc.cause)
             return
         pretty = json.dumps(data, indent=4, sort_keys=True)
         if len(pretty) > 2000:
