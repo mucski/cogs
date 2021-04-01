53620d4367a7ebd1d5a89451e210cff0fb068163
mucski <music166@gmail.com>
2021-04-01 17:32:39 +0200

    testing

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -92,8 +92,6 @@ class Paladins(commands.Cog):
     @commands.command()
     @checks.is_owner()
     async def hirez(self, ctx, request, msg=None):
-        if not msg:
-            request = request.strip()
         data = await self.api.request(request, msg)
         pretty = json.dumps(data, indent=4, sort_keys=True)
         await ctx.send("```json\n" + pretty + "```")
