a33c5f695c524f6e44508f1e8f22e8773684bba0
Mucski <music166@gmail.com>
2021-04-04 20:24:00 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -139,8 +139,8 @@ class Paladins(commands.Cog):
         champions_stats = await player.get_champion_stats()
         stats_dict = {s.champion: s for s in champions_stats}  # Dict[Champion, ChampionStats]
         entry = await self.api.get_champion_info()
-        champ = entry.champions.get(champion)
         if champion is not None:
+            champ = entry.champions.get(champion)
             if champ is None:
                 print("You dun fucked up the champ's name!")
                 return
