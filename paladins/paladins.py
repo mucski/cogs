f0e1cb18596e93af25f066a9734f792fadababe7
Mucski <music166@gmail.com>
2021-04-04 19:34:31 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -137,7 +137,7 @@ class Paladins(commands.Cog):
         player_obj = await self.api.search_players(player, platform)
         player = await player_obj[0]
         champions_obj = await player.get_champion_stats()
-        entry = api.get_entry()
+        entry = self.api.get_entry()
         if champion is not None:
             stats_dict = {s.champion: s for s in champions_obj}  # Dict[Champion, ChampionStats]
             champ = entry.champions.get(champion)
