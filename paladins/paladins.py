b53d883d4b25c48f2bfb979fcecc59a01c65982f
Mucski <music166@gmail.com>
2021-04-05 13:34:04 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -148,20 +148,19 @@ class Paladins(commands.Cog):
             if stats is None:
                 print("You ain't played this champ yet!")
                 return
-            await ctx.send(stats.level)
             desc = (
                 f"Champion role: {champ.role}\n"
-                f"Level: {stats.level}\n"
+                f"Champion level: {stats.level}\n"
                 f"Winrate: {stats.kda_text} ({stats.winrate_text})\n"
                 f"Matches played: {stats.matches_played}\n"
-                f"Playtime: {humanize.naturaltime(stats.playtime.total_hours())}\n"
+                f"Playtime: {stats.playtime.total_hours()}\n"
                 f"Experience: {stats.experience}\n"
                 f"Last played: {humanize.naturaltime(datetime.utcnow() - stats.last_played)}\n"
             )
             e = discord.Embed(color=await self.bot.get_embed_color(ctx), title=f"{champ.name} ({champ.title})")
             e.set_thumbnail(url=champ.icon_url)
             e.description = desc
-            e.set_footer(text=f"stats")
+            e.set_footer(text=f"Individual champion stats for {player.name}")
             await ctx.send(embed=e)
         else:
             await ctx.send(champ.name)
