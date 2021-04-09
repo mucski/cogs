c1401d656f4c48fb869ddb9c3ec253bf3192e097
Mucski <music166@gmail.com>
2021-04-09 19:37:41 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -56,45 +56,36 @@ class Paladins(commands.Cog):
             temp = match.bans
             for match_player in match.players:
                 if match_player.team_number == 1:
-                    if match_player == "":
+                    if match_player.player.private:
                         rank = "99"
-                        team1_data.append(["Bot", "0", "0", "0/0/0", "0", "0", "0", "0", "0", "0", "0", "0"])
-                        team1_champs.append("Jenos")
-                        team1_ranks.append(rank)
                     else:
-                        if match_player.player.private:
-                            rank = "99"
-                        else:
-                            rank = match_player.player.ranked_best.rank.value
-                        team1_data.append([match_player.player.name, match_player.account_level, match_player.credits, match_player.kda_text,
-                                           match_player.damage_done, match_player.damage_taken,
-                                           match_player.objective_time, match_player.damage_mitigated,
-                                           match_player.healing_done, match_player.party_number, match_player.player.platform, match_player.healing_self])
-                        team1_champs.append(match_player.champion.name)
-                        team1_ranks.append(rank)
+                        rank = match_player.player.ranked_best.rank.value
+                    team1_data.append([match_player.player.name, match_player.account_level, match_player.credits, match_player.kda_text,
+                                       match_player.damage_done, match_player.damage_taken,
+                                       match_player.objective_time, match_player.damage_mitigated,
+                                       match_player.healing_done, match_player.party_number, match_player.player.platform, match_player.healing_self])
+                    team1_champs.append(match_player.champion.name)
+                    team1_ranks.append(rank)
                 else:
-                    if match_player == "":
+                    if match_player.player.private:
                         rank = "99"
-                        team2_data.append(["Bot", "0", "0", "0/0/0", "0", "0", "0", "0", "0", "0", "0", "0"])
-                        team2_champs.append("Jenos")
-                        team2_ranks.append(rank)
                     else:
-                        if match_player.player.private:
-                            rank = "99"
-                        else:
-                            rank = match_player.player.ranked_best.rank.value
-                        team2_data.append([match_player.player.name, match_player.account_level, match_player.credits, match_player.kda_text,
-                                           match_player.damage_done, match_player.damage_taken,
-                                           match_player.objective_time, match_player.damage_mitigated,
-                                           match_player.healing_done, match_player.party_number, match_player.player.platform, match_player.healing_self])
-                        team2_champs.append(match_player.champion.name)
-                        team2_ranks.append(rank)
+                        rank = match_player.player.ranked_best.rank.value
+                    team2_data.append([match_player.player.name, match_player.account_level, match_player.credits, match_player.kda_text,
+                                       match_player.damage_done, match_player.damage_taken,
+                                       match_player.objective_time, match_player.damage_mitigated,
+                                       match_player.healing_done, match_player.party_number, match_player.player.platform, match_player.healing_self])
+                    team2_champs.append(match_player.champion.name)
+                    team2_ranks.append(rank)
             buffer = await helper.history_image(team1_champs, team2_champs, team1_data, team2_data, team1_ranks,
                                                 team2_ranks, (match_info + temp))
             file = discord.File(filename=f"{matchid}.png", fp=buffer)
         await ctx.send(file=file)
         for player in match.players:
-            await ctx.send(player)
+            if not player:
+                await ctx.("Bot")
+            else:
+                await ctx.send(player)
 
     @commands.command()
     async def last(self, ctx, player: Union[discord.Member, str] = None, platform="PC"):
