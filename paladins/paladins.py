7f2824bc2abd9c7e7c5943719f250c6463ecba35
Mucski <music166@gmail.com>
2021-04-09 19:26:41 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -56,16 +56,22 @@ class Paladins(commands.Cog):
             temp = match.bans
             for match_player in match.players:
                 if match_player.team_number == 1:
-                    if match_player.player.private:
+                  if not match_player.team1:
                         rank = "99"
+                        team1_data.append(["Bot", "0", "0", "0/0/0", "0", "0", "0", "0", "0", "0", "0", "0"])
+                        team1_champs.append("Jenos")
+                        team1_ranks.append(rank)
                     else:
-                        rank = match_player.player.ranked_best.rank.value
-                    team1_data.append([match_player.player.name, match_player.account_level, match_player.credits, match_player.kda_text,
-                                       match_player.damage_done, match_player.damage_taken,
-                                       match_player.objective_time, match_player.damage_mitigated,
-                                       match_player.healing_done, match_player.party_number, match_player.player.platform, match_player.healing_self])
-                    team1_champs.append(match_player.champion.name)
-                    team1_ranks.append(rank)
+                        if match_player.player.private:
+                            rank = "99"
+                        else:
+                            rank = match_player.player.ranked_best.rank.value
+                        team1_data.append([match_player.player.name, match_player.account_level, match_player.credits, match_player.kda_text,
+                                           match_player.damage_done, match_player.damage_taken,
+                                           match_player.objective_time, match_player.damage_mitigated,
+                                           match_player.healing_done, match_player.party_number, match_player.player.platform, match_player.healing_self])
+                        team1_champs.append(match_player.champion.name)
+                        team1_ranks.append(rank)
                 else:
                     if not match_player.team2:
                         rank = "99"
