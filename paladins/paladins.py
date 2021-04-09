7beba23a98cc7a19a7188fc3e5e97e73a3c1160e
mucski <music166@gmail.com>
2021-04-09 13:18:40 +0200

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -125,7 +125,7 @@ class Paladins(commands.Cog):
     @checks.is_owner()
     async def testing(self, ctx, player: discord.Member = None, platform="PC"):
         platform = arez.Platform(platform)
-        if player.mention in message.content.split():
+        if player.mention in ctx.message.content.split():
             platform = "Discord"
             player = player.id
             ret = await self.api.get_from_platform(player, platform)
