5b782beb3fb181610ccefbc5d6ede6f2e6a82ec7
mucski <music166@gmail.com>
2021-04-14 13:02:17 +0200

    update paladins.py

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -384,6 +384,7 @@ class Paladins(commands.Cog):
         Paladins server statuses
         """
         status = await self.api.get_server_status()
+        e = discord.Embed(title="Paladins Server Status", description=desc)
         for k, v in status.statuses.items():
             if v.up:
                 server = "Operational"
@@ -396,5 +397,4 @@ class Paladins(commands.Cog):
                 f"{server}\n"
                 f"{v.version}\n"
             )
-            e = discord.Embed(title="Paladins Server Status", description=desc)
-            await ctx.send(embed=e)
+        await ctx.send(embed=e)
