7ce9e18d01912653171276546f7fc7128ac447f5
mucski <music166@gmail.com>
2021-04-17 10:01:51 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -8,3 +8,16 @@ class helper2:
     @classmethod
     async def testing(cls):
         return "Hello World"
+        
+    @classmethod
+    async def get_champ_icon(cls, name):
+        ready = name.name
+        champ = ready.lower()
+        if "bomb" in champ:
+            champ = "bomb-king"
+        if "sha" in champ:
+            champ = "sha-lin"
+        if "mal" in champ:
+            champ = "maldamba"
+        url = f"https://webcdn.hirezstudios.com/paladins/champion-icons/{champ}.jpg"
+        return url
