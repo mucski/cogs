7291e09135e8c4608b9315f40d09115340a2acef
mucski <music166@gmail.com>
2021-04-17 09:53:29 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -2,3 +2,9 @@ from PIL import ImageOps, ImageDraw, Image, ImageFont
 import aiohttp
 from io import BytesIO
 from redbot.core.utils.chat_formatting import humanize_number
+
+class helper2:
+    
+    @classmethod
+    async def testing(cls):
+        return "Hello World"
