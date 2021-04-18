6e1726f25164113fa9386c316dc8122a7611880a
mucski <music166@gmail.com>
2021-04-18 16:35:55 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -1,6 +1,7 @@
 from PIL import ImageOps, ImageDraw, Image, ImageFont
 import aiohttp
 from io import BytesIO
+import requests
 from redbot.core.utils.chat_formatting import humanize_number
 
 class helper2:
