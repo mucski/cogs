58fda9313b2959d34c0b6d2b61c3c961a908e32f
mucski <music166@gmail.com>
2021-04-18 14:59:02 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -27,7 +27,7 @@ class helper2:
         imagesize_x = 1080
         imagesize_y = 1920
         color = (14, 34, 43)
-        img = Image.new('RGBA', (imagesize_x, imagesize_y), color=color)
+        img = Image.new('RGBA', (imagesize_y, imagesize_x), color=color)
         draw = ImageDraw.Draw(img)
         
         # Final image product
