ab9760b51aa907884fee43d52260a199b6bfa793
mucski <music166@gmail.com>
2021-04-22 17:36:21 +0200

    update test.py

--- test/test.py
+++ test/test.py
@@ -76,10 +76,11 @@ class Test(commands.Cog):
         output = subprocess.run(cmd, stdout=subprocess.PIPE, text=True, shell=True, stderr=subprocess.STDOUT)
         #subprocess = subprocess.Popen(shell = True, stdout = subprocess.PIPE)
         #output = subprocess.stdout.read()
-        if len(output.stdout) > 2000:
+        output = output.stdout
+        if len(output) > 2000:
             f = StringIO(output.stdout)
             f.seek(0)
             file = discord.File(filename="output.txt", fp=f)
             await ctx.send(file=file)
         else:
-            await ctx.send("```\n" + output.stdout + "\n```")
+            await ctx.send("```\n" + output + "\n```")
