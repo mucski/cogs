1822a46e7639fe6d21d2f5ec8b010e00d5becf50
mucski <music166@gmail.com>
2021-04-25 05:19:34 +0200

    update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -6,12 +6,3 @@ class Utilities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=["emoji"])
-    async def emote(self, ctx, emoji: discord.PartialEmoji):
-        await ctx.send(emoji.url)
-
-    @commands.command()
-    async def avatar(self, ctx, member: discord.Member = None):
-        if member is None:
-            member = ctx.author
-        await ctx.send(member.avatar_url)
