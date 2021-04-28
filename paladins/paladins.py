e071e6fc68d725c4126769d4b2ee7fd688e79e9f
mucski <music166@gmail.com>
2021-04-28 20:38:23 +0200

    update paladins.py

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -353,7 +353,7 @@ class Paladins(commands.Cog):
         for live_player in live_match.team1:
             if live_player.player.private:
                 team1 += f"Player: ?????(???)\n"
-                team1 += f"Champion: {live_player.champion.name}({live_player.champion.level})\n"
+                team1 += f"Champion: {live_player.champion.name}({live_player.mastery_level})\n"
                 team1 += f"Winrate: {live_player.player.casual.winrate_text}\n"
             else:
                 team1 += f"Player: {live_player.player.name}({live_player.account_level})\n"
