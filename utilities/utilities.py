147c1ed15fd65149f961ce15f1ddf20fa694658e
mucski <music166@gmail.com>
2021-05-02 16:06:36 +0200

    update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -25,7 +25,7 @@ class Utilities(commands.Cog):
         if len(flag) < 3:
             comp = flags.get(flag.lower())
         else:
-            
+            comp = flags.get(flag.items().lower)
 
         def deEmojify(text):
             regrex_pattern = re.compile(
