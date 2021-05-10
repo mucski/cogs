8e453a80c19385da9f97d4e332af84743c657b0c
mucski <music166@gmail.com>
2021-05-10 16:36:01 +0200

    update helper.py

--- paladins/helper.py
+++ paladins/helper.py
@@ -230,16 +230,16 @@ class helper:
         fnt100 = ImageFont.truetype("home/ubuntu/arial.ttf", 100)
         fnt100bold = ImageFont.truetype("/home/ubuntu/arialbd.ttf", 100)
         draw_panel.text((rs, 0 + ds),
-                        str(f"ID: {md[0]}"), font=fnt100, fill=fill, stroke_width=1, stroke_fille=stroke)
+                        str(f"ID: {md[0]}"), font=fnt100, fill=fill, stroke_width=1, stroke_fill=stroke)
         draw_panel.text((rs, 100 + ds), (str(md[1]) + " minutes"), font=fnt100,
-                        fill=fill, stroke_width=1, stroke_fille=stroke)
+                        fill=fill, stroke_width=1, stroke_fill=stroke)
         draw_panel.text((rs, 200 + ds),
-                        str(md[2]), font=fnt100, fill=fill, stroke_width=1, stroke_fille=stroke)
+                        str(md[2]), font=fnt100, fill=fill, stroke_width=1, stroke_fill=stroke)
         draw_panel.text((rs, 300 + ds), str(map_name),
                         font=fnt100, fill=fill, stroke_width=1, stroke_fille=stroke)
         # Team 1
         draw_panel.text((512 * 4 - 230, ds), f"Team 1 Score: {md[4]}",
-                        font=fnt100bold, fill=(76, 104, 245))
+                        font=fnt100bold, fill=(76, 104, 245), stroke_width=4, stroke_fill=fill)
         center = (512/2 - 130/2)
         center2 = (512/2 - 80/2)
         # VS
@@ -251,13 +251,13 @@ class helper:
                         # font=fnt100bold, fill=(227, 34, 34))
         # Team 2
         draw_panel.text((512 * 6 - 200, 348), f"Team 2 Score: {md[5]}",
-                        font=fnt100bold, fill=(245, 76, 76))
+                        font=fnt100bold, fill=(245, 76, 76), stroke_width=4, stroke_fill=fill)
         #  add in banned champs if it's a ranked match
         try:
             if md[6]:
                 # Ranked bans
                 draw_panel.text((512 * 9 + 100, center2),
-                                "Bans:", font=fnt100, fill=fill, stroke_width=1, stroke_fille=stroke)
+                                "Bans:", font=fnt100, fill=fill, stroke_width=1, stroke_fill=stroke)
                 # Team 1 Bans
                 try:
                     resp = await helper.get_champ_icon(md[6].name)
