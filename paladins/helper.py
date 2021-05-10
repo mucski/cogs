afab2d41b640e206f8b84ac88188dcfd24fa23d1
mucski <music166@gmail.com>
2021-05-10 16:15:57 +0200

    update helper.py

--- paladins/helper.py
+++ paladins/helper.py
@@ -245,7 +245,7 @@ class helper:
         # VS
         vs = Image.open("home/ubuntu/icons/vs.png")
         vs = vs.convert("RGBA")
-        vs = vs.putdata(0,0,0,0,255)
+        vs = vs.putdata((0,0,0,0,255))
         vs = vs.resize((200, 200))
         vs = vs.putalpha(128)
         middle_panel.paste(vs, (512 * 5 + 300 - 200, 150), mask=vs)
