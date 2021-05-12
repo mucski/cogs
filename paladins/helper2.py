027901f9853133961697f71071b0837af18076f8
mucski <music166@gmail.com>
2021-05-12 19:52:58 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -58,9 +58,9 @@ class helper2:
         #make the image drawable
         draw = ImageDraw.Draw(img)
         #normal font
-        fnt = ImageFont.truetype("/home/ubuntu/arial.ttf", 80)
+        fnt = ImageFont.truetype("home/ubuntu/arial.ttf", 80)
         #bold font
-        fntbld = ImageFont.truetype("/home/ubuntu/arialbd.ttf", 80)
+        fntbld = ImageFont.truetype("home/ubuntu/arialbd.ttf", 80)
         
         #player name and level
         draw.text((img_x, middle - 40), str(stats[0]), font=fntbld, color=fill)
@@ -92,7 +92,7 @@ class helper2:
         key = Image.new("RGB", (x * 10, y), color = (8, 21, 25))
         draw = ImageDraw.Draw(key)
         fill = (255, 255, 255)
-        fntbld = ImageFont.truetype("/home/ubuntu/arialbld.ttf", 80)
+        fntbld = ImageFont.truetype("home/ubuntu/arialbld.ttf", 80)
         
         #champion and player
         draw.text((20, 0), "Champion", font = fntbld, fill = fill)
