98c9734512e845502faae3c549e12380a6c54e00
mucski <music166@gmail.com>
2021-05-12 21:53:59 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -168,11 +168,11 @@ class helper2:
             playerpanel = await helper2.statsimage(champimgcrop, rankicon, t1_data[i], i)
             img.paste(playerpanel, (0, math.floor(img_y * i + img_x * 3 + img_x / 4)))
         #done, reisizing for speed
-        #historyimg = img.resize((1920, 1080), Image.ANTIALIAS)
+        historyimg = img.resize((1920, 1080), Image.ANTIALIAS)
         #create the buffer
         final_buffer = BytesIO()
         #store image in buffer
-        img.save(final_buffer, "PNG")
+        historyimg.save(final_buffer, "PNG")
         #seek back to start
         final_buffer.seek(0)
         return final_buffer
