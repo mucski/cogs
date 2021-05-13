dfc26632e78883a30f0b139a73e7c4a009b94d49
mucski <music166@gmail.com>
2021-05-13 14:57:55 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -210,7 +210,7 @@ class helper2:
         
         vs = Image.open("home/ubuntu/icons/vs.png")
         w, h = vs.size
-        vs.resize((int(w / 2), int(h / 2)))
+        vs.resize((int(w / 4), int(h / 4)))
         img.paste(vs, (int((W-w) / 2), int((H-h) / 2)), mask = vs)
         
         return img
