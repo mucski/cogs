e7332c9bc9ed39677f34b6b43ddd06ff784a7e24
mucski <music166@gmail.com>
2021-05-13 13:20:53 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -49,7 +49,7 @@ class helper2:
         #text fill size 
         fill = (255, 255, 255)
         #new image object
-        img = Image.new("RGBA", (W * 9 + W / 2, H), color = img_color)
+        img = Image.new("RGBA", (W * 9 + 256, H), color = img_color)
         #champion icon
         img.paste(champicon, (padding, padding))
         #rank icon
