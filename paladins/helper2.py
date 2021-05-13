b0c8c1af6031493c6a144533ce86ed95a8461e72
mucski <music166@gmail.com>
2021-05-13 13:01:32 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -67,7 +67,7 @@ class helper2:
     
         
         #credits earned
-        draw.text((W * 3 + 242, mid), humanize_number(stats[2]), font=fnt, color=fill)
+        draw.text((W * 3 + W / 2, mid), humanize_number(stats[2]), font=fnt, color=fill)
         #kda
         draw.text((W * 4, mid), stats[3], font=fnt, color=fill)
         #dmg done
