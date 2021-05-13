bc5206c7efffe892c348638c91d3db7424ab96fe
mucski <music166@gmail.com>
2021-05-13 11:49:55 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -140,7 +140,7 @@ class helper2:
         
         #middle panel
         middle = await helper2.middlepanel(match_data)
-        img.paste(middle, (0, int(img_y * 13 / 2 - crop)))
+        img.paste(middle, (0, int(img_y * 13 / 2 - img_x / 2)))
         
         #player data
         for i, (champ, champ2) in enumerate(zip(team1, team2)):
