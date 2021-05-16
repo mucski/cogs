ee48520df0b8eb66c9c7414111bfcc86c746414b
mucski <music166@gmail.com>
2021-05-16 14:19:50 +0200

    update helper2.py

--- paladins/helper2.py
+++ paladins/helper2.py
@@ -246,26 +246,30 @@ class helper2:
         #ranked bans
         try:
             if match_data[6]:
+                banned1 = match_data[6].name
+                banned2 = match_data[7].name
+                banned3 = match_data[8].name
+                banned4 = match_data[9].name
                 draw.text((int((W-w) / 2) + 1720, int((H-h) / 2) + 80), "Bans", font = fnt, stroke_width = stroke_size, stroke_fill = stroke, fill = fill)
                 # team 1 bans
                 #champ 1
-                resp = await helper2.champimg(match_data[6])
+                resp = await helper2.champimg(banned1)
                 champ_icon = Image.open(BytesIO(resp))
                 champ_icon = champ_icon.resize((200, 200))
                 img.paste(champ_icon, (int((W-w) / 2) + 2020, int((H-h)/ 2) - 70))
                 #champ 2
-                resp = await helper2.champimg(match_data[7])
+                resp = await helper2.champimg(banned2)
                 champ_icon = Image.open(BytesIO(resp))
                 champ_icon = champ_icon.resize((200, 200))
                 img.paste(champ_icon, (int((W-w) / 2) + 2240, int((H-h)/ 2) - 70))
                 # team 2 bans 
                 #champ 1
-                resp = await helper2.champimg(match_data[8])
+                resp = await helper2.champimg(banned3)
                 champ_icon = Image.open(BytesIO(resp))
                 champ_icon = champ_icon.resize((200, 200))
                 img.paste(champ_icon, (int((W-w) / 2) + 2020, int((H-h)/ 2) + 150))
                 #champ 2
-                resp = await helper2.champimg(match_data[9])
+                resp = await helper2.champimg(banned4)
                 champ_icon = Image.open(BytesIO(resp))
                 champ_icon = champ_icon.resize((200, 200))
                 img.paste(champ_icon, (int((W-w) / 2) + 2240, int((H-h)/ 2) + 150))
