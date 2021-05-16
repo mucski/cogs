d17759dd261269a1116590ca7d94b5ce84b7c7c0
mucski <music166@gmail.com>
2021-05-16 14:34:38 +0200

    update helper.py

--- paladins/helper.py
+++ paladins/helper.py
@@ -43,7 +43,7 @@ class helper:
         #padding or margin size
         padding = 10
         #middle
-        mid = int((H - 100) / 2)
+        mid = int((H - 120) / 2)
         #image background color odd and even
         img_color = (14, 34, 43) if index % 2 == 0 else (15, 40, 48)
         #text fill size )
