c172baabecc1d9affac75e85007b77ccd78417fe
mucski <music166@gmail.com>
2021-05-25 20:34:18 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -35,7 +35,7 @@ class TTSCog(commands.Cog):
         
     @commands.command()
     @checks.is_owner()
-    async def setttslanguage(self, ctx, lang):
+    async def setttslang(self, ctx, lang):
         await self.db.guild(ctx.guild).lang.set(lang)
         await ctx.send(f"TTS language set to {lang}")
     
