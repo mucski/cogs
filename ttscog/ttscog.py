abe2f1119f17645ebd7ef1aa218d2d6f2c416038
mucski <music166@gmail.com>
2021-05-25 17:49:57 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -31,43 +31,43 @@ class TTSCog(commands.Cog):
         await ctx.send(f"TTS channel has been set to {channel.name}")
         
     #@commands.command()
-async def on_message(self, msg: discord.Message):
-    channel = await self.db.guild(msg.guild).channel()
-    if not channel:
-        return
-    if not msg.channel.id == channel:
-        return
-    # channel = self.bot.get_channel(channel)
-
-    if msg.author in self._locks:
-        # their message being processed
-        return
-    try:
-        self._locks.append(msg.author)
-        
-        vc = msg.voice_client # We use it more then once, so make it an easy variable
-        if not vc:
-            # We are not currently in a voice channel
-            await mag.channel.send("I need to be in a voice channel to do this, please use the connect command.")
+    async def on_message(self, msg: discord.Message):
+        channel = await self.db.guild(msg.guild).channel()
+        if not channel:
             return
-        
-        # Lets prepare our text, and then save the audio file
-        fp = BytesIO()
-        tts = gTTS(text=f"{msg.author.name} said {text}", lang="en")
-        tts.write_to_fp(fp)
-        fp.seek(0)
-        # tts.save("text.mp3")
-        
+        if not msg.channel.id == channel:
+            return
+        # channel = self.bot.get_channel(channel)
+    
+        if msg.author in self._locks:
+            # their message being processed
+            return
         try:
-            # Lets play that mp3 file in the voice channel
-            vc.play(FFmpegPCMAudio(fp.read(), pipe = True), after=lambda e: print(f"Finished playing: {e}"))
-        
-            # Lets set the volume to 1
-            vc.source = discord.PCMVolumeTransformer(vc.source)
-            vc.source.volume = 1
-        except:
-            await msg.channel.send("An error occured.")
-        
-    finally:
-        await self._locks.discard(msg.author)
-    bot.process_commands(msg)
+            self._locks.append(msg.author)
+            
+            vc = msg.voice_client # We use it more then once, so make it an easy variable
+            if not vc:
+                # We are not currently in a voice channel
+                await mag.channel.send("I need to be in a voice channel to do this, please use the connect command.")
+                return
+            
+            # Lets prepare our text, and then save the audio file
+            fp = BytesIO()
+            tts = gTTS(text=f"{msg.author.name} said {text}", lang="en")
+            tts.write_to_fp(fp)
+            fp.seek(0)
+            # tts.save("text.mp3")
+            
+            try:
+                # Lets play that mp3 file in the voice channel
+                vc.play(FFmpegPCMAudio(fp.read(), pipe = True), after=lambda e: print(f"Finished playing: {e}"))
+            
+                # Lets set the volume to 1
+                vc.source = discord.PCMVolumeTransformer(vc.source)
+                vc.source.volume = 1
+            except:
+                await msg.channel.send("An error occured.")
+            
+        finally:
+            await self._locks.discard(msg.author)
+        bot.process_commands(msg)
