e21b31c92766184ca28e51db07d051096b9a9319
mucski <music166@gmail.com>
2021-05-25 15:07:24 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -48,7 +48,7 @@ class TTSCog(commands.Cog):
     
         try:
             # Lets play that mp3 file in the voice channel
-            vc.play(discord.FFmpegPCMAudio(fp), after=lambda e: print(f"Finished playing: {e}"))
+            vc.play(discord.FFmpegPCMAudio(fp, pipe=True), after=lambda e: print(f"Finished playing: {e}"))
     
             # Lets set the volume to 1
             vc.source = discord.PCMVolumeTransformer(vc.source)
