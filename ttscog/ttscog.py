334cced8ad9c7aea3c32d3f35e02bd34231cf1bb
mucski <music166@gmail.com>
2021-05-25 18:24:12 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -30,7 +30,7 @@ class TTSCog(commands.Cog):
     async def setttschan(self, ctx, channel: discord.TextChannel):
         await self.db.guild(ctx.guild).channel.set(channel.id)
         await ctx.send(f"TTS channel has been set to {channel.name}")
-        
+    
     #@commands.command()
     @commands.Cog.listener()
     async def on_message(self, ctx, msg: discord.Message):
