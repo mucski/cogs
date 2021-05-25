17ebf30a95e3ae8c2b1b8da0e93ef5003050df28
mucski <music166@gmail.com>
2021-05-25 18:20:24 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -35,8 +35,6 @@ class TTSCog(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message):
         channel = await self.db.guild(msg.guild).channel()
-        if not channel:
-            return
         #channel = 830384640568066069
         if msg.channel.id != channel:
             return
