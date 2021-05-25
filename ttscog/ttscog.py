e2b3c63c48d232d195c4b6fa58319fc7e1f42e50
mucski <music166@gmail.com>
2021-05-25 14:32:48 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -3,3 +3,43 @@ from redbot.core import commands, checks
 
 class TTSCog(commands.Cog):
     
+    @commands.command()
+    async def repeat(ctx, *, text=None):
+        """
+        A command which saves `text` into a speech file with
+        gtts and then plays it back in the current voice channel.
+    
+        Params:
+         - text [Optional]
+            This will be the text we speak in the voice channel
+        """
+        if not text:
+            # We have nothing to speak
+            await ctx.send(f"Hey {ctx.author.mention}, I need to know what to say please.")
+            return
+    
+        vc = ctx.voice_client # We use it more then once, so make it an easy variable
+        if not vc:
+            # We are not currently in a voice channel
+            await ctx.send("I need to be in a voice channel to do this, please use the connect command.")
+            return
+    
+        # Lets prepare our text, and then save the audio file
+        tts = gTTS(text=text, lang="en")
+        tts.save("text.mp3")
+    
+        try:
+            # Lets play that mp3 file in the voice channel
+            vc.play(discord.FFmpegPCMAudio('text.mp3'), after=lambda e: print(f"Finished playing: {e}"))
+    
+            # Lets set the volume to 1
+            vc.source = discord.PCMVolumeTransformer(vc.source)
+            vc.source.volume = 1
+    
+        # Handle the exceptions that can occur
+        except ClientException as e:
+            await ctx.send(f"A client exception occured:\n`{e}`")
+        except TypeError as e:
+            await ctx.send(f"TypeError exception:\n`{e}`")
+        except OpusNotLoaded as e:
+            await ctx.send(f"OpusNotLoaded exception:\n`{e}`")
