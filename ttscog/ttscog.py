784099a988daf4962cc5338f8315d75ea46b9994
mucski <music166@gmail.com>
2021-05-25 18:21:43 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -46,7 +46,7 @@ class TTSCog(commands.Cog):
         try:
             self._locks.append(msg.author)
             
-            vc = msg.voice_client # We use it more then once, so make it an easy variable
+            vc = ctx.voice_client # We use it more then once, so make it an easy variable
             if not vc:
                 # We are not currently in a voice channel
                 await mag.channel.send("I need to be in a voice channel to do this, please use the connect command.")
