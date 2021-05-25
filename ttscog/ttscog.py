5b395c4520cd7ab2e25e87666ea6cdbf4fffb1f8
mucski <music166@gmail.com>
2021-05-25 23:33:45 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -126,7 +126,7 @@ class TTSCog(commands.Cog):
     async def message_check(self, channel):
         channel = self.bot.get_channel(channel)
         async for message in channel.history(limit=5):
-            delta = datetime.datetime.utcnow() - message.timestamp
+            delta = datetime.datetime.utcnow() - message.created_at
             if delta.total_seconds() < 5 and message.author.id != self.bot.user.id:
                 vc = message.guild.voice_client
     
