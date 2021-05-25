ec0bf28a46a060bfd5dcdb14b6ba1495fe4711a2
mucski <music166@gmail.com>
2021-05-25 14:26:36 +0200

    update helper.py

--- ttscog/helper.py
+++ ttscog/helper.py
@@ -1,1 +1,36 @@
 
+class helper:
+
+    @classmethod
+    async def connect(cls, *, channel: discord.VoiceChannel=None):
+        """
+        Connect to a voice channel
+        This command also handles moving the bot to different channels.
+    
+        Params:
+        - channel: discord.VoiceChannel [Optional]
+            The channel to connect to. If a channel is not specified, an attempt to join the voice channel you are in
+            will be made.
+        """
+        if not channel:
+            try:
+                channel = ctx.author.voice.channel
+            except AttributeError:
+                raise InvalidVoiceChannel('No channel to join. Please either specify a valid channel or join one.')
+    
+        vc = ctx.voice_client
+    
+        if vc:
+            if vc.channel.id == channel.id:
+                return
+            try:
+                await vc.move_to(channel)
+            except asyncio.TimeoutError:
+                raise VoiceConnectionError(f'Moving to channel: <{channel}> timed out.')
+        else:
+            try:
+                await channel.connect()
+            except asyncio.TimeoutError:
+                raise VoiceConnectionError(f'Connecting to channel: <{channel}> timed out.')
+    
+        await ctx.send(f'Connected to: **{channel}**', delete_after=20)
