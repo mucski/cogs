565c1ace1aa8ad4aab04860e9fc04e10be5c9411
mucski <music166@gmail.com>
2021-05-25 15:18:40 +0200

    update custom.py

--- ttscog/custom.py
+++ ttscog/custom.py
@@ -3,6 +3,7 @@ import shlex
 import io
 from discord.opus import Encoder
 import discord
+import ffmpeg
 
 class FFmpegPCMAudio(discord.AudioSource):
     def __init__(self, source, *, executable='ffmpeg', pipe=False, stderr=None, before_options=None, options=None):
