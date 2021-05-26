cedfd8e2f3091837c3e04a2ece934a93b7119f9f
mucski <music166@gmail.com>
2021-05-26 02:15:28 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -24,24 +24,13 @@ class TTSCog(commands.Cog):
 
     @commands.command()
     async def connect(self, ctx, channel: discord.VoiceChannel=None):
-        """
-        Connect to a voice channel
-        This command also handles moving the bot to different channels.
-    
-        Params:
-        - channel: discord.VoiceChannel [Optional]
-            The channel to connect to. If a channel is not specified, an attempt to join the voice channel you are in
-            will be made.
-        """
         if not channel:
             try:
                 channel = ctx.author.voice.channel
             except AttributeError:
                 await ctx.send('No voice channel to join. Please either specify a valid voice channel or join one.')
                 return
-    
         vc = ctx.voice_client
-    
         if vc:
             if vc.channel.id == channel.id:
                 return
@@ -56,13 +45,7 @@ class TTSCog(commands.Cog):
             except asyncio.TimeoutError:
                 await ctx.send(f'Connecting to channel: <{channel}> timed out.')
                 return
-    
         await ctx.send(f'Connected to: **{channel}**', delete_after=20)
-        
-        
-    @commands.command()
-    async def disconnect(self, ctx):
-        await helper.disconnect(ctx)
         
     @commands.command()
     @checks.is_owner()
@@ -86,7 +69,6 @@ class TTSCog(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, msg: discord.Message):
         channel = await self.db.guild(msg.guild).channel()
-        self.load_check = self.bot.loop.create_task(self.message_check(channel))
         if msg.channel.id != channel:
             return
         if msg.author == self.bot.user:
@@ -120,34 +102,23 @@ class TTSCog(commands.Cog):
                 # Lets set the volume to 1
                 vc.source = discord.PCMVolumeTransformer(vc.source)
                 vc.source.volume = 100
-            #except:
-                #await msg.channel.send("Please wait for me to finish speaking.")
-            except Exception:
-                await msg.channel.send(traceback.format_exc())
+            except:
+                await msg.channel.send("Please wait for me to finish speaking.")
+            #except Exception:
+                #await msg.channel.send(traceback.format_exc())
         finally:
             self._locks.remove(msg.author)
             
-    async def message_check(self, channel):
-        channel = self.bot.get_channel(channel)
-        async for msg in channel.history(limit=1):
-            created = msg.created_at
-            now = datetime.now()
-            timer = timedelta(seconds=5)
-            future = timer + now
-            future = future.timestamp()
-            temp_stamp = datetime.fromtimestamp(future)
-            remaining_timedelta = temp_stamp - now
-            remaining = remaining_timedelta.total_seconds()
-            if created != now and msg.author != self.bot.user:
-                await asyncio.sleep(remaining)
-                vc = msg.guild.voice_client
-                #if not vc:
-                    #await msg.channel.send("I am not in a voice channel.")
-                    #return
+            
+    @commands.command()
+    async def disconnect(self, ctx):
+        vc = msg.guild.voice_client
+        if not vc:
+            await msg.channel.send("I am not in a voice channel.")
+            return
         
-                await vc.disconnect()
-                await msg.channel.send("No one is talking, so bye 👋")
-                self.load_check.cancel()
+        await vc.disconnect()
+        await ctx.send("No one is talking, so bye 👋")
                 
     def cog_unload(self):
         self.load_check.cancel()
