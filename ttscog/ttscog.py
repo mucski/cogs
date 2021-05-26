e23f2a9e4fbbb2e74acf27b7f4250850b1db2136
mucski <music166@gmail.com>
2021-05-26 12:13:00 +0200

    update ttscog.py

--- ttscog/ttscog.py
+++ ttscog/ttscog.py
@@ -67,7 +67,7 @@ class TTSCog(commands.Cog):
     async def ttsnick(self, ctx, msg):
         if msg == "True":
             msg = bool(True)
-        elif fsg == "False:
+        elif fsg == "False":
             msg = bool(False)
         else:
             await ctx.send("Please input a valid True or False statement.")
