c8e0836ac61b1da3c3c8ea7377ec7baae4477998
mucski <music166@gmail.com>
2021-05-27 14:05:33 +0200

    test

--- sfx/sfx.py
+++ sfx/sfx.py
@@ -63,7 +63,7 @@ class SFX(commands.Cog):
         tts_audio = gtts.gTTS(text, lang=lang)
         #audio_file = os.path.join(tempfile.gettempdir(), ''.join(random.choice('0123456789ABCDEF') for i in range(12)) + '.mp3')
         #tts_audio.save(audio_file)
-        fp = BytesIO(y['data'])
+        fp = BytesIO()
         tts_audio.write_to_fp(fp)
         #fp.seek(0)
         audio_data = pydub.AudioSegment.from_mp3(fp).export(x, format="mp3")
