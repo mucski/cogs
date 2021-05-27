3c4a55b15dfcadc183bfe2b2a1e5d3efdb08d629
mucski <music166@gmail.com>
2021-05-27 14:08:15 +0200

    test

--- sfx/sfx.py
+++ sfx/sfx.py
@@ -66,7 +66,7 @@ class SFX(commands.Cog):
         fp = BytesIO()
         tts_audio.write_to_fp(fp)
         fp.seek(0)
-        audio_data = pydub.AudioSegment.from_mp3(fp).export(x, format="mp3")
+        audio_data = pydub.AudioSegment.from_mp3(fp).export(fp, format="mp3")
         silence = pydub.AudioSegment.silent(duration=cfg_padding)
         padded_audio = silence + audio_data + silence
         padded_audio.export(fp, format='mp3')
