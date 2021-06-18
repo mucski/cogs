f6b6a9d9c06a36e42b9374c7d9e4824bb224513a
mucski <music166@gmail.com>
2021-06-18 23:09:25 +0200

    update sfx.py

--- sfx/sfx.py
+++ sfx/sfx.py
@@ -123,8 +123,8 @@ class SFX(commands.Cog):
             if with_nick == "on":
                 #text = re.sub(r'<@\d+>', '', msg.content)
                 text = re.sub(r'<a:.+?:\d+>|<:.+?:\d+>', '', msg.content)
-                text = re.sub(r'<(?::\w+:|@!*&*|#)[0-9]+>', '', msg.content)
-                sentence = f"{msg.author.name} says {text}"
+                text2 = re.sub(r'<(?::\w+:|@!*&*|#)[0-9]+>', '', text)
+                sentence = f"{msg.author.name} says {text2}"
             elif with_nick == "off":
                 sentence = f"{msg.content}"
             else:
