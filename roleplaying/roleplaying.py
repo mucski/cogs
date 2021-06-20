f3ff7a4f7f0e11df477404f9852708556b955323
mucski <music166@gmail.com>
2021-06-20 13:36:15 +0200

    Update roleplaying.py

--- roleplaying/roleplaying.py
+++ roleplaying/roleplaying.py
@@ -30,74 +30,36 @@ class Roleplaying(commands.Cog):
 
     @commands.command()
     async def punch(self, ctx, member: discord.Member = None):
-        img = random.choice(punchlist)
-        e = discord.Embed()
-        e.set_image(url=img)
-        e.set_author(name=f"{ctx.author.display_name} punches:",
-                     icon_url=ctx.author.avatar_url)
-        if member is None:
-            e.set_footer(text="the air.")
-        else:
-            e.set_footer(text=member.display_name)
-        await ctx.send(embed=e)
+        author = ctx.author
+        embed = await self.img_grab(punchlist, "punches", author, member)
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def cuddle(self, ctx, member: discord.Member = None):
-        img = random.choice(cuddlelist)
-        e = discord.Embed()
-        e.set_image(url=img)
-        e.set_author(name=f"{ctx.author.display_name} cudles with:",
-                     icon_url=ctx.author.avatar_url)
-        if member is None:
-            e.set_footer(text="the air.")
-        else:
-            e.set_footer(text=member.display_name)
-        await ctx.send(embed=e)
+        author = ctx.author
+        embed = await self.img_grab(cuddlelist, "cuddles", author, member)
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def hug(self, ctx, member: discord.Member = None):
-        img = random.choice(huglist)
-        e = discord.Embed()
-        e.set_image(url=img)
-        e.set_author(name=f"{ctx.author.display_name} hugs:",
-                     icon_url=ctx.author.avatar_url)
-        if member is None:
-            e.set_footer(text="the air.")
-        else:
-            e.set_footer(text=member.display_name)
-        await ctx.send(embed=e)
+        author = ctx.author
+        embed = await self.img_grab(huglist, "hugs", author, member)
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def pat(self, ctx, member: discord.Member = None):
-        img = random.choice(patlist)
-        e = discord.Embed()
-        e.set_image(url=img)
-        e.set_author(name=f"{ctx.author.display_name} pats:",
-                     icon_url=ctx.author.avatar_url)
-        if member is None:
-            e.set_footer(text="the air.")
-        else:
-            e.set_footer(text=member.display_name)
-        await ctx.send(embed=e)
+        author = ctx.author
+        embed = await self.img_grab(patlist, "pats", author, member)
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def slap(self, ctx, member: discord.Member = None):
-        img = random.choice(slaplist)
-        e = discord.Embed()
-        e.set_image(url=img)
-        e.set_author(name=f"{ctx.author.display_name} slaps",
-                     icon_url=ctx.author.avatar_url)
-        if member is None:
-            e.set_footer(text="the air.")
-        else:
-            e.set_footer(text=member.display_name)
-        await ctx.send(embed=e)
+        author = ctx.author
+        embed = await self.img_grab(slaplist, "slaps", author, member)
+        await ctx.send(embed=embed)
 
     @commands.command()
     async def sad(self, ctx):
-        img = random.choice(sadlist)
-        e = discord.Embed()
-        e.set_image(url=img)
-        e.set_author(name=f"{ctx.author.display_name} is sad.",
-                     icon_url=ctx.author.avatar_url)
-        await ctx.send(embed=e)
+        author = ctx.author
+        embed = await self.img_grab(sadlist, "sad", author, member)
+        await ctx.send(embed=embed)
