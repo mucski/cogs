a2b94c5b5437b3a67a3be654348ce9d6f44e093e
mucski <music166@gmail.com>
2021-06-23 15:21:26 +0200

    Update tunnel.py

--- tunnel/tunnel.py
+++ tunnel/tunnel.py
@@ -1,1 +1,15 @@
+from redbot.core import commands, checks
+import discord
 
+
+#start of awesome script
+
+class Tunnel(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	@commands.command()
+	async def tunnel(self, ctx, message):
+		channel = self.bot.get_channel(779860372190396447)
+		await channel.send(message)
