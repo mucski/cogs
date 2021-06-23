7d0bf91c30dfc6bf978f8ffbc2a708eebbe992f8
Mucski <music166@gmail.com>
2021-06-23 16:12:17 +0200

    test

--- tunnel/tunnel.py
+++ tunnel/tunnel.py
@@ -2,20 +2,13 @@ from redbot.core import commands, checks
 import discord
 
 
-#start of awesome script
+# start of awesome script
 
 class Tunnel(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
-		
+
 	@commands.command()
 	async def tunnel(self, ctx, *, message):
 		channel = self.bot.get_channel(779860372190396447)
 		await channel.send(f"{ctx.author.mention} sennt {message}")
-
-	@commands.Cog.listener()
-    async def on_message(self, msg: discord.Message):
-		#sendus
-		msg = await bot.wait_for("message", check=lambda msg: msg.channel.id in (779860372190396447, 830384640568066069))
-		#send the message.
-		await msg.channel.send(msg.content)
