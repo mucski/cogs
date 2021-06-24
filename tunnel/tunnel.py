0d08b66d621e0699d7a7152303559e412b78be70
Mucski <music166@gmail.com>
2021-06-24 21:26:38 +0200

    test

--- tunnel/tunnel.py
+++ tunnel/tunnel.py
@@ -8,12 +8,12 @@ class Tunnel(commands.Cog):
 		self.bot = bot
 
 	@commands.Cog.listener()
-	async def on_message(self, *, msg):
+	async def on_message(self, msg):
 		if not (msg.channel.id in (779860372190396447, 830384640568066069)):
 			return
 		if msg.channel.id == 779860372190396447:
 			channel = 830384640568066069
-			await channel.send(msg)
+			await channel.send(msg.content)
 		if msg.channel.id == 830384640568066069:
 			channel = 779860372190396447
-			await channel.send(msg)
+			await channel.send(msg.content)
