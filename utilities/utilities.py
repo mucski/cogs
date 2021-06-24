b8a24a776047fb52781aa11b897bb6ae5c8f9129
mucski <music166@gmail.com>
2021-06-24 12:09:41 +0200

    update utilities.py

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -115,7 +115,7 @@ class Utilities(commands.Cog):
             "Or join my support discord server:"
         )
         e = discord.Embed(title=f"{self.bot.user.display_name}'s info", color=await self.bot.get_embed_color(ctx), description=desc)
-        await ctx.send(embed=e,content="https://discord.gg/qt2tXUrH")
+        await ctx.send(embed=e,content="https://dsc.gg/mucski")
         
     def cog_unload(self):
         self.bot.add_command("info")
