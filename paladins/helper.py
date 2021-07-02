d050dbba8f9a5d946cf0f6594310d940762894f5
mucski <music166@gmail.com>
2021-07-02 13:42:37 +0100

    fixes and updates

--- paladins/helper.py
+++ paladins/helper.py
@@ -159,13 +159,15 @@ class helper:
         middle = await helper.middlepanel(match_data)
         img.paste(middle, (0, int(H / 2 - 200)))
         # player data
-        team_data = t1_data + t2_data
-        rank_data = r1 + r2
         for team_num, team in enumerate((team1, team2), start=1):
             if team_num == 1:
                 offset = 100
+                rank = r1
+                team_data = t1_data
             else:
                 offset = 1772
+                rank = r2
+                team_data = t2_data
             for i, champ in enumerate(team):
                 # team 1
                 resp = await helper.champimg(champ)
@@ -180,7 +182,7 @@ class helper:
                 border = (0, crop, 0, crop)
                 champimgcrop = ImageOps.crop(champimg, border)
                 # rank icon
-                rankicon = Image.open(f"root/mucski/stuff/icons/ranks/{rank_data[i]}.png")
+                rankicon = Image.open(f"root/mucski/stuff/icons/ranks/{rank[i]}.png")
                 # playerstats
                 playerpanel = await helper.statsimage(champimgcrop, rankicon, team_data[i], i)
                 img.paste(playerpanel, (0, 232 * i + offset))
