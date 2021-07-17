003a13fb4d6e7c70f4681af02abc65f41835d4fe
mucski <music166@gmail.com>
2021-07-17 18:32:25 +0200

    test

--- snapchatchan/snapchatchan.py
+++ snapchatchan/snapchatchan.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext.commands.core import check
 from redbot.core import commands, Config, checks
 from .taskhelper import TaskHelper
 import asyncio
@@ -55,7 +56,7 @@ class SnapChatChan(TaskHelper, commands.Cog):
             exclude = await self.conf.guild(guild).exclude()
             delete_limit = exclude = await self.conf.guild(guild).delete_limit()
             channel = self.bot.get_channel(chan)
-            await channel.purge(limit=delete_limit)
+            await channel.purge(limit=delete_limit, check=lambda x: x.id == await channel.fetch_message(exclude))
             self.schedule_task(self._timer(loop_second))
 
     async def _timer(self, loop_second):
