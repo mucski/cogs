02904edcb9150ddcd77d16e7760a4ed4f48b7bee
mucski <music166@gmail.com>
2021-07-17 18:33:34 +0200

    test

--- snapchatchan/snapchatchan.py
+++ snapchatchan/snapchatchan.py
@@ -56,7 +56,8 @@ class SnapChatChan(TaskHelper, commands.Cog):
             exclude = await self.conf.guild(guild).exclude()
             delete_limit = exclude = await self.conf.guild(guild).delete_limit()
             channel = self.bot.get_channel(chan)
-            await channel.purge(limit=delete_limit, check=lambda x: x.id == await channel.fetch_message(exclude))
+            msg = await channel.fetch_message(exclude)
+            await channel.purge(limit=delete_limit, check=lambda x: x.id == msg)
             self.schedule_task(self._timer(loop_second))
 
     async def _timer(self, loop_second):
