f1a7a3bfeb0b7add16585bb49a6f466e5513a085
mucski <music166@gmail.com>
2022-02-17 21:51:40 +0100

    Test

--- paladins/helper.py
+++ paladins/helper.py
@@ -31,7 +31,7 @@ class helper:
             name = "sha-lin"
         if "mal" in name:
             name = "maldamba"
-        entry = await self.api.get_champion_info()
+        entry = await api.get_champion_info()
         champ = entry.champions.get(name)
         return champ.icon_url
 
