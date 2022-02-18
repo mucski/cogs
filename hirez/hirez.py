59043585bcece91195ffeb877b491247866229cb
mucski <music166@gmail.com>
2022-02-18 19:28:24 +0100

    Test

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -14,6 +14,7 @@ from collections import Counter
 from .helper import helper
 import types
 from PIL import ImageOps, ImageDraw, Image, ImageFont, ImageEnhance
+import BytesIO
 
 
 class HiRez(commands.Cog):
