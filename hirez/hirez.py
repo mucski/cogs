39efe3999b172abbef2fa73c96ada4966ab98089
mucski <music166@gmail.com>
2022-02-19 00:12:43 +0100

    test

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -323,7 +323,8 @@ class HiRez(commands.Cog):
             yoffset = (team_num - 1) * 1782  # replace 1000 with whatever offset you'll need
             team = getattr(match, f"team{team_num}")
             for i, mp in enumerate(team):
-                y = i * 50 + yoffset  # replace 50 with whatever row height you use
+                # y = i * 0 + yoffset  # replace 50 with whatever row height you use
+                y = i + yoffset
                 row = helper.statsimage(mp, i)  # your current playerkey
                 img.paste(row, (0, 232 * i + y))
                 # base.paste(row, 0, y)
