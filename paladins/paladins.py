104a66628a2743b6abd8049ea4e25489e83f439c
mucski <music166@gmail.com>
2022-02-18 12:30:40 +0100

    test

--- paladins/paladins.py
+++ paladins/paladins.py
@@ -530,7 +530,7 @@ class Paladins(commands.Cog):
                 name = champ.name
                 async with session.get(url) as resp:
                     if resp.status == 200:
-                        f = await aiofiles.open(f'root/mucski/stuff/icons/avatars/{name}.jpg', mode='wb')
+                        f = await aiofiles.open(f'root/mucski/stuff/icons/avatars/{name.lower().replace(" ","-").replace("'","")}.jpg', mode='wb')
                         await f.write(await resp.read())
                         await f.close()
         await ctx.tick()
