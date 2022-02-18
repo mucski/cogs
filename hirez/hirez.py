076f1e91dab114a61d2212b4ef8053f2629397fe
mucski <music166@gmail.com>
2022-02-18 17:48:02 +0100

    Test

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -26,3 +26,51 @@ class HiRez(commands.Cog):
         self.auth = self.f.readline()
         self.devid = self.f.readline()
         self.api = arez.PaladinsAPI(dev_id=self.devid.strip(), auth_key=self.auth.strip())
+
+    def cog_unload(self):
+        asyncio.createTask(self.api.close())
+        self.f.close()
+
+    async def cog_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandInvokeError):
+            exc = error.original
+            if isinstance(exc, arez.Unavailable):
+                await ctx.send("```\nHiRez API is unavailable.\n```")
+                return
+            if isinstance(exc, arez.Private):
+                await ctx.send("```\nRequested profile is set to private\n```")
+                return
+            if isinstance(exc, arez.NotFound):
+                await ctx.send("```\nNot found!\n```")
+                return
+            if isinstance(exc, aiohttp.ClientResponseError):
+                await ctx.send("```\nTimed out. Try again in a minute\n```")
+            if isinstance(exc, arez.exceptions.HTTPException):
+                await ctx.send("```\nSomething went wrong, try again with another user/id/name or try agian later\n```")
+        await ctx.bot.on_command_error(ctx, error, unhandled_by_cog=True)
+
+    @commands.command()
+    async def status(self, ctx):
+        """Paladins server statuses
+        Green means online, red means offline, yellow means limited access
+        """
+        status = await self.api.get_server_status()
+        stringus = ""
+        for k, v in status.statuses.items():
+            if v.status == "Operational":
+                server = "🟢"
+            elif v.status == "Limited Access":
+                server = "🟡"
+            else:
+                server = "🔴"
+            desc = (
+                "```\n"
+                f"Platform: {v.platform}\n"
+                f"Status: {server} {v.status}\n"
+                f"Version: {v.version}\n\n"
+                "```"
+            )
+            stringus += desc
+        e = discord.Embed(title="Paladins Server Status", color=await self.bot.get_embed_color(ctx), description=stringus)
+        e.set_footer(text=f"Current time: {status.timestamp.strftime('%c')}")
+        await ctx.send(embed=e)
