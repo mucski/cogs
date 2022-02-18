b15269732855c88925ab7f22eb31e474fc6ca49d
mucski <music166@gmail.com>
2022-02-18 17:43:47 +0100

    Lol uno reverse

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -1,5 +1,28 @@
-from hirez import HiRez
+import arez
+from redbot.core import checks, commands
+import asyncio
+import humanize
+from datetime import datetime
+import discord
+from redbot.core.utils.chat_formatting import pagify, text_to_file
+import aiohttp
+import aiofiles
+import json
+import math
+from tabulate import tabulate
+from collections import Counter
 
 
-def setup(bot):
-    bot.add_cog(HiRez(bot))
+class HiRez(commands.Cog):
+    """Paladins stats cog by Mucski
+    For a better experience you should link yout discord account to hirez
+    that way you can use most commands without typing anything else but the command itself
+
+    example: [p]champstats
+    """
+    def __init__(self, bot):
+        self.bot = bot
+        self.f = open("/root/mucski/stuff/key.txt", "r")
+        self.auth = self.f.readline()
+        self.devid = self.f.readline()
+        self.api = arez.PaladinsAPI(dev_id=self.devid.strip(), auth_key=self.auth.strip())
