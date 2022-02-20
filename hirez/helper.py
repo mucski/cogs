58d87aff50479153b3c177d9ebbc18a9e2742004
mucski <music166@gmail.com>
2022-02-20 15:09:27 +0100

    test

--- hirez/helper.py
+++ hirez/helper.py
@@ -202,7 +202,6 @@ def middlepanel(match):
 
     draw.text((round(W / 2 + 173), 300 + padding), f"Team 2 score: {match.score[1]}", font=fnt, stroke_width=stroke_size, stroke_fill=stroke, fill=fill)
 
-    ranked bans go here
     if mp.bans:
         draw.text((round((W-w) / 2) + 1520, round((H-h) / 2) + 80), "Bans", font=fnt, stroke_width=stroke_size, stroke_fill=stroke, fill=fill)
 
