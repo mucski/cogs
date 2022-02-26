463d987663a9a95e0718460f7816239ffd419b54
mucski <music166@gmail.com>
2022-02-26 19:31:46 +0100

    test

--- hirez/helper.py
+++ hirez/helper.py
@@ -280,7 +280,7 @@ def generatecard(player):
     draw.text((33, 531), f"Acc Created: {humanize.naturaltime(datetime.utcnow() - player.created_at)}", font=fnt, stroke_width=stroke_size, stroke_fill=stroke, fill=fill)
     draw.text((33, 588), f"Last Login: {humanize.naturaltime(datetime.utcnow() - player.last_login)}", font=fnt, stroke_width=stroke_size, stroke_fill=stroke, fill=fill)
     divider = Image.open("root/mucski/stuff/icons/divider.png").convert("RGBA")
-    img.paste(divider, (180, 645), mask=divider)
+    img.paste(divider, (180, 665), mask=divider)
     final_buffer = BytesIO()
     img.save(final_buffer, "PNG")
     final_buffer.seek(0)
