805fdb53ef7ea4844e4ce891dd54b92ff22ddda4
mucski <music166@gmail.com>
2022-03-05 11:03:01 +0100

    test

--- hirez/helper.py
+++ hirez/helper.py
@@ -276,8 +276,12 @@ async def generatecard(player):
     stroke_size = 1
     # name
     kda = await get_kda_guru(player.id)
+    if kda:
+        gkda = kda[3]
+    else:
+        "?"
     draw.text((33, 211), f"{player.name}", font=fnt_big, stroke_width=stroke_size, stroke_fill=stroke, fill=(180, 160, 138))
-    draw.text((33, 277), f"{player.title}  - (Global KDA: {kda[3]})", font=fnt_small, stroke_width=stroke_size, stroke_fill=stroke, fill=(223, 142, 53))
+    draw.text((33, 277), f"{player.title}  - (Global KDA: {gkda})", font=fnt_small, stroke_width=stroke_size, stroke_fill=stroke, fill=(223, 142, 53))
     draw.text((33, 360), f"Level: {player.calculated_level}", font=fnt, stroke_width=stroke_size, stroke_fill=stroke, fill=fill)
     draw.text((33, 417), f"Region: {player.region}", font=fnt, stroke_width=stroke_size, stroke_fill=stroke, fill=fill)
     draw.text((33, 474), f"Champions Owned: {player.champion_count}", font=fnt, stroke_width=stroke_size, stroke_fill=stroke, fill=fill)
