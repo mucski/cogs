203c83f83cae9e52e8e3e5b87cf17ed79bd340c5
mucski <music166@gmail.com>
2022-03-05 10:20:51 +0100

    test

--- hirez/helper.py
+++ hirez/helper.py
@@ -306,6 +306,6 @@ async def get_kda_guru(player): # this input must be the player ID
                 raw = await resp.text()
                 soup = BeautifulSoup(raw, 'html.parser')
                 stats = []
-                for i in soup.find_all("div", class="tsw__grid__stat"):
+                for i in soup.find_all("div", class_="tsw__grid__stat"):
                     stats.append(i)
                 return stats
