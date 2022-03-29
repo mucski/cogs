8e5bcccead8714a2b46749c9b90ae0d274933bb7
mucski <music166@gmail.com>
2022-03-30 01:13:16 +0200

    update sfx.py

--- sfx/sfx.py
+++ sfx/sfx.py
@@ -11,12 +11,12 @@ from redbot.core import commands, checks, Config
 from .custom import FFmpegPCMAudio
 
 # Gevent patch before gTTS
-try:
-    import gevent
-    gevent.monkey.patch_all()
-except ModuleNotFoundError:
-    pass
-from gtts import gTTS
+#try:
+#    import gevent
+#    gevent.monkey.patch_all()
+#except ModuleNotFoundError:
+#    pass
+#from gtts import gTTS
 
 
 class TTSItem(NamedTuple):
