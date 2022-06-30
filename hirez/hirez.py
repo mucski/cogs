d6fe58ede61d851baf63240767641fe07d0d8e6a
mucski <music166@gmail.com>
2022-06-30 23:07:43 +0200

    update hirez.py

--- hirez/hirez.py
+++ hirez/hirez.py
@@ -351,7 +351,8 @@ class HiRez(commands.Cog):
                 await ctx.send("```\nNo recent matches found. (History is only kept for 30 days)\n```")
                 return
             match = await match_list[0]
-            pic = helper.format_match(match)
+            player = await match.player
+            pic = helper.format_match(match, player)
             file = discord.File(filename=f"{player}.png", fp=pic)
             await ctx.send(file=file)
             # await ctx.send(match.bans)
