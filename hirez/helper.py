e8c6e26c43fe0483c46c7097e1299c0fd9b98872
mucski <music166@gmail.com>
2022-06-30 23:26:05 +0200

    update helper.py

--- hirez/helper.py
+++ hirez/helper.py
@@ -78,8 +78,10 @@ def statsimage(mp, index):
         name = "???"
     # player name and level
     draw.text((512 + padding * 4, mid - 30), name, font=fntbld, fill=color)
-    draw.text((512 + padding * 4, mid + 60), humanize_number(mp.calculated_level), font=smallfnt, fill=fill)
-
+    try:
+        draw.text((512 + padding * 4, mid + 60), humanize_number(mp.calculated_level), font=smallfnt, fill=fill)
+    except AttributeError:
+        draw.text((512 + padding * 4, mid + 60), humanize_number(mp.account_level), font=smallfnt, fill=fill)
     # credits earned
     draw.text((1736, mid), humanize_number(mp.credits), font=fnt, fill=fill)
     # kda
