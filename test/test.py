1b63baf2695e5ab21ddb2f56686b12ae5ea3580c
Mucski <music166@gmail.com>
2022-09-05 21:21:40 +0200

    test

--- test/test.py
+++ test/test.py
@@ -6,16 +6,9 @@ class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.slash_commands = {"guilds": {}}
-        self.fuck = discord.app_commands.ContextMenu(
-            name="Play on Spotify", callback=self.suck_dicks
+        self.react = discord.app_commands.ContextMenu(
+            name="Play on Spotify", callback=self.react
         )
 
-    async def suck_dicks(self, interaction: discord.Interaction, message: discord.Message):
-        queue = interaction.command.name == "Suck Mucski's Dick"
-        user = interaction.user
-        ctx = await self.bot.get_context(interaction)
-        await ctx.defer(ephemeral=True)
-        user_token = await self.get_user_auth(ctx, user)
-        if not user_token:
-            return
+    async def react(interaction: discord.Interaction, message: discord.Message):
         await interaction.response.send_message('Very cool message!', ephemeral=True)
