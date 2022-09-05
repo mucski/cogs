189fbaadb4d1828738f02855c78a6074b3c4f23c
Mucski <music166@gmail.com>
2022-09-05 10:29:11 +0200

    test

--- test/test.py
+++ test/test.py
@@ -1,12 +1,12 @@
 import discord
 from discord import app_commands
-from redbot.core import commands, checks, Config
+from redbot.core import commands
 
 
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.context_menu(discord.Member)
+    @app_commands.context_menu(name="Testing")
     async def react(interaction: discord.Interaction, message: discord.Message, user: discord.Member):
         await interaction.response.send_message(f'Hello {user}, Ya fucking cunt!...', ephemeral=True)
