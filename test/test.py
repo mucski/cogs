56685fc307099212f440707d70c49bbd99b54926
Mucski <music166@gmail.com>
2022-09-05 10:19:02 +0200

    tset

--- test/test.py
+++ test/test.py
@@ -7,6 +7,6 @@ class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @app_commands.context_menu()
+    @app_commands.context_menu(discord.Member)
     async def react(interaction: discord.Interaction, message: discord.Message, user: discord.Member):
         await interaction.response.send_message(f'Hello {user}, Ya fucking cunt!...', ephemeral=True)
