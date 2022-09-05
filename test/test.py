8ed6298c2c0fb591ee6d8ba1fc8aaf6f0c082ce0
Mucski <music166@gmail.com>
2022-09-05 10:02:42 +0200

    test

--- test/test.py
+++ test/test.py
@@ -8,10 +8,10 @@ class Test(commands.Cog):
 
     slash_commands = app_commands.Group(
         name="test",
-        description=_("Some Testing Slash Commands"),
+        description="Some Testing Slash Commands",
     )
 
-    @slash_commands.command(name="test", description=_("List all playlists you have access to on the invoked context"))
+    @slash_commands.command(name="test", description="List all playlists you have access to on the invoked context")
     @app_commands.guild_only()
     async def slash_commands_test(self, interaction: InteractionT):
         await ctx.send("hello world")
