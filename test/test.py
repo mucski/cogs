0f03ea5306d98b68e039a4eb94b16a65d74f24c2
Mucski <music166@gmail.com>
2022-09-05 21:00:07 +0200

    test

--- test/test.py
+++ test/test.py
@@ -1,14 +1,19 @@
 import discord
-from discord import app_commands
 from redbot.core import commands
-
 
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.tree = app_commands.CommandTree(self)
+        self.fuck = discord.app_commands.ContextMenu(
+            name="Play on Spotify", callback=self.play_from_message
+        )
 
-    @tree.command()
-    async def hello(interaction: discord.Interaction):
-        """Says hello!"""
-        await interaction.response.send_message(f'Hi, {interaction.user.mention}')
+    async def suck_dicks(self, interaction: discord.Interaction, message: discord.Message):
+        queue = interaction.command.name == "Suck Mucski's Dick"
+        user = interaction.user
+        ctx = await self.bot.get_context(interaction)
+        await ctx.defer(ephemeral=True)
+        user_token = await self.get_user_auth(ctx, user)
+        if not user_token:
+            return
+        await ctx.send("You Successfully Sucked Mucski's Cock")
