a77bcd54405f9e9d9af845af459b52993326afd8
Mucski <music166@gmail.com>
2022-09-05 21:26:24 +0200

    test

--- test/test.py
+++ test/test.py
@@ -5,10 +5,11 @@ from redbot.core import commands
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.slash_commands = {"guilds": {}}
         self.react = discord.app_commands.ContextMenu(
             name="Play on Spotify", callback=self.react
         )
 
     async def react(self, interaction: discord.Interaction, message: discord.Message):
         await interaction.response.send_message('Very cool message!', ephemeral=True)
+
+    await bot.tree.sync()
