52bd1de8ac5b09371e3fa3a30e5fc9d6b628e496
Mucski <music166@gmail.com>
2022-09-05 21:42:00 +0200

    test

--- test/test.py
+++ test/test.py
@@ -6,13 +6,21 @@ class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.react = discord.app_commands.ContextMenu(
-            name="Play on Spotify", callback=self.react
+            name="React", callback=self.react_callback
+        )
+        self.user = discord.app_commands.ContextMenu(
+            name="User", callback=self.user_callback
         )
 
-    async def react(self, interaction: discord.Interaction, message: discord.Message):
+    async def react_callback(self, interaction: discord.Interaction, message: discord.Message):
         await interaction.response.send_message('Very cool message!', ephemeral=True)
+
+    async def user_callback(self, interaction: discord.Interaction, user: discord.User):
+        await interaction.response.send_message('Very cool message on a user!', ephemeral=True)
         
     @commands.command()
     async def sync_commands(self, ctx):
+        self.bot.tree.add_command(self.react)
+        self.bot.tree.add_command(self.user)
         await self.bot.tree.sync()
         await ctx.tick()
