534a9caec65491a1ef956504b57ca3fc1049e238
Mucski <music166@gmail.com>
2022-09-05 09:43:31 +0200

    test

--- test/test.py
+++ test/test.py
@@ -4,6 +4,11 @@ class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    slash_commands = app_commands.Group(
+        name="test",
+        description=_("Some Testing Slash Commands"),
+    )
+
     @slash_commands.command(name="test", description=_("List all playlists you have access to on the invoked context"))
     @app_commands.guild_only()
     async def slash_commands_test(self, interaction: InteractionT):
