38476c03fef363e4ddd6900501486b74fcf9a64f
Mucski <music166@gmail.com>
2022-09-05 09:26:58 +0200

    Updated file

--- test/test.py
+++ test/test.py
@@ -3,3 +3,7 @@ from redbot.core import commands, checks, Config
 class Test(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @bot.tree.context_menu(name='say')
+    async def say(interaction: discord.Interaction, member: discord.Member):
+        await interaction.response.send_message('I said something!')
