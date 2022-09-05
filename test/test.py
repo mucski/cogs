4c7e3d5be7dca2b6f973690901d9bdab69660dca
Mucski <music166@gmail.com>
2022-09-05 09:45:49 +0200

    test

--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
-from redbot.core import commands, checks, Config
 import discord
 from discord import app_commands
+from redbot.core import commands, checks, Config
 
 class Test(commands.Cog):
     def __init__(self, bot):
