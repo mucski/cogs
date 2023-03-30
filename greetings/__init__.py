ee5f1c6684f23ba8f2bf8d20461a9cd7a1de42ed
Mucski <music166@gmail.com>
2023-03-30 21:20:44 +0200

    Created the Greetings cog

--- greetings/__init__.py
+++ greetings/__init__.py
@@ -2,4 +2,4 @@ from .greetings import Greetings
 
 
 async def setup(bot):
-    bot.add_cog(Greetings(bot))
+    await bot.add_cog(Greetings(bot))
---
+++ greetings/greetings.py
@@ -0,0 +1,13 @@
+
+# import discord
+from redbot.core import commands
+
+
+class Greetings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = self.bot.get_channel(971361677348044862)  # the channel ID
+        await channel.send(f"Welcome to Mystic Valley {member.mention}, enjoy your stay. Please do read the rules in {self.bot.get_channel(971361336594403368).mention}")
