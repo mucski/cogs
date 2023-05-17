b08064fc877fc59c55f64beba0630cebb812d7af
Mucski <mucski@live.com>
2023-05-17 15:53:57 +0200

    test

--- sfx/sfx.py
+++ sfx/sfx.py
@@ -28,16 +28,16 @@ class SelectSpeed(discord.ui.View):
     @discord.ui.select(
         placeholder="Select how fast the bot should talk",
         options=[
-            discord.SelectOption(label="0.1", value=0.1),
-            discord.SelectOption(label="0.2", value=0.2),
-            discord.SelectOption(label="0.3", value=0.3),
+            discord.SelectOption(label="0.1", value="0.1"),
+            discord.SelectOption(label="0.2", value="0.2"),
+            discord.SelectOption(label="0.3", value="0.3"),
         ]
     )
     async def _speed_callback(self, select, interaction):
         """
         Changes playback speed. Any speed between 0.5 and 2.0 is supported.
         """
-        await self.db.guild(interaction.guild).speed.set(select.value)
+        await self.db.guild(interaction.guild).speed.set(int(select.value))
         await interaction.response.send_message("TTS speed has been set to {select.value}")
 
 
