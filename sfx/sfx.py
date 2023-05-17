c1762a7006812b9260138ea2c1911bbfeb780a0d
Mucski <mucski@live.com>
2023-05-17 14:53:34 +0200

    test

--- sfx/sfx.py
+++ sfx/sfx.py
@@ -59,7 +59,7 @@ class SFX(commands.Cog):
                 )
                 return
             channel = voice_state.channel
-        vc: Optional[discord.VoiceClient] = interaction.client.voice_client
+        vc: Optional[discord.VoiceClient] = interaction.user.voice_client
         if vc is not None:
             # move to the channel
             if vc.channel.id == channel.id:
