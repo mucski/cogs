3cacd18b13067ae914bb58eaf03db29ec93754fd
mucski <music166@gmail.com>
2023-10-10 13:31:03 +0200

    update coin/coin.py

--- coin/coin.py
+++ coin/coin.py
@@ -349,8 +349,8 @@ class Coin(commands.Cog):
             
             if msg.content.lower() == "h":
                 hit(player_hand)
-                await ctx.send("Player hand: " + str(player_hand))
-                await ctx.send("Hand total " + str(total(player_hand)))
+                # await ctx.send("Player hand: " + str(player_hand))
+                # await ctx.send("Hand total " + str(total(player_hand)))
                 if total(player_hand) == 21:
                     await ctx.send("Congratulation, you have a Black Jack! You win 500 coins!")
                     coins += 500
@@ -361,26 +361,26 @@ class Coin(commands.Cog):
                     break
                 await ctx.send("Do you want to [H]it, [S]tand or [Q]uit")
             elif msg.content.lower() == "s":
-                while True:
+                while total(dealer_hand) < 17:
                     hit(dealer_hand)
-                    await ctx.send("Dealer hand: " + str(dealer_hand))
-                    await ctx.send("Dealer total: " + str(total(dealer_hand)))
-                    if total(dealer_hand) == 21:
-                        await ctx.send("Ha! I have BlackJack, I win!")
-                        break
-                    elif total(dealer_hand) > 21:
-                        await ctx.send("Oops, looks like you won, 100 coins")
-                        coins += 100
-                        await self.db.user(ctx.author).coin.set(coins)
-                        break
-                    elif total(dealer_hand) < total(player_hand):
-                        await ctx.send("Looks like you won, congrats, 100 coins")
-                        coins += 100
-                        await self.db.user(ctx.author).coin.set(coins)
-                        break
-                    elif total(dealer_hand) > total(player_hand):
-                        await ctx.send("Looks like you lost, sorry")
-                        break
+                await ctx.send("Dealer hand: " + str(dealer_hand))
+                await ctx.send("Dealer total: " + str(total(dealer_hand)))
+                if total(dealer_hand) == 21:
+                    await ctx.send("Ha! I have BlackJack, I win!")
+                    break
+                elif total(dealer_hand) > 21:
+                    await ctx.send("Oops, looks like you won, 100 coins")
+                    coins += 100
+                    await self.db.user(ctx.author).coin.set(coins)
+                    break
+                elif total(dealer_hand) < total(player_hand):
+                    await ctx.send("Looks like you won, congrats, 100 coins")
+                    coins += 100
+                    await self.db.user(ctx.author).coin.set(coins)
+                    break
+                elif total(dealer_hand) > total(player_hand):
+                    await ctx.send("Looks like you lost, sorry")
+                    break
             elif msg.content.lower() == "q":
                 break
                 await ctx.send("Forfait counts as a loss, bye")
